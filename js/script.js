645b7597a3b868968388cef1a2b4cce8ab8eaac8
shmulim <shmuel.markel@gmail.com>
2016-02-14 23:30:18 -0800

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 function replaceNouns() {
+    $.get('noun.html', function(data){
+      var nounsArray = data.split('\n');
+      jQuery('.noun').each(function(){
+        noun = nounsArray[Math.floor(Math.random() * nounsArray.length)];
+        jQuery(this).text(noun);
+       });
+    }).fail(function(error){
+      alert('Something went wrong. Error: ' + error);
+    });
 };
 
 function replaceVerbs() {
+    $.get('verb.html', function(data){
+      var verbsArray = data.split('\n');
+      jQuery('.verb').each(function(){
+        verb = verbsArray[Math.floor(Math.random() * verbsArray.length)];
+        jQuery(this).text(verb);
+      });
+    }).fail(function(error){
+      alert('Something went wrong. Error: ' + error);
+    });
 };
 
 $(document).ready(function (){
-  // Code here
+  jQuery('#random_noun').click(replaceNouns);
+  jQuery('#random_verb').click(replaceVerbs);
 });
