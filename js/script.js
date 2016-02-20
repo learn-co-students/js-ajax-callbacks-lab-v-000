280faa8c25abb46033882182de50c869e3acce0a
Mitul Mistry <mitul.mistry1@gmail.com>
2016-02-20 01:21:04 +0000

    working

--- js/script.js
+++ js/script.js
@@ -1,9 +1,42 @@
+"use strict";
+
 function replaceNouns() {
-};
+  $('#random_noun').on('click', function(){
+    //callback:
+    $.get("noun.html", function(response) {
+      var nouns = response.trim().split('\n');
+
+      $('.noun').each(function(index, item){
+        $(this).html(nouns[randomInt(0, nouns.length - 1)]);
+      });
+    })
+    .fail(function(error) {
+      console.log('Something went wrong: ' + error);
+    });
+  });
+}
 
 function replaceVerbs() {
-};
+  $('#random_verb').on('click', function(){
+    //callback:
+    $.get("verb.html", function(response) {
+      var verbs = response.trim().split('\n');
+
+      $('.verb').each(function(index, item){
+        $(this).html(verbs[randomInt(0, verbs.length - 1)]);
+      });
+    })
+    .fail(function(error) {
+      console.log('Something went wrong: ' + error);
+    });
+  });
+}
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 $(document).ready(function (){
-  // Code here
+  replaceNouns();
+  replaceVerbs();
 });
