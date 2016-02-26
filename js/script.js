e41c431e524551001ae30cb0c8fa061e56c5246b
jakebrady5 <jakebrady5@gmail.com>
2016-02-25 23:02:43 -0500

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
 function replaceNouns() {
+  $.get("noun.html", function(response) {
+    $('.noun').each(function() {
+      var words = response.trim().split("\n");
+      var random = Math.floor(Math.random() * words.length);
+      $(this).html(words[random]);
+    });
+  }).fail(function(error) {
+    console.log('Something went wrong: ' + error);
+  });
 };
 
 function replaceVerbs() {
+  $.get('verb.html', function(response) {
+    $('.verb').each(function() {
+      var words = response.trim().split('\n');
+      var random = Math.floor(Math.random() * words.length);
+      $(this).html(words[random]);
+    });
+  }).fail(function(error) {
+    console.log('Something went wrong: ' + error);
+  });
 };
 
 $(document).ready(function (){
-  // Code here
+  $('input#random_noun').click(replaceNouns);
+  $('input#random_verb').click(replaceVerbs);
+  //$('#random_verb').click(replaceVerbs);
+  //$('input#random_verb').click(replaceVerbs);
 });
