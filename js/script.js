69fd465a0fde1fec890f3f00e935244b512bb279
Justin Thomas <jthomas0209@gmail.com>
2016-03-07 07:52:59 -0500

    Done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 function replaceNouns() {
+  $.get('noun.html',function(response){
+    var nouns = response.split('\n');
+    $('.noun').html(nouns[Math.floor ( Math.random() * nouns.length )]);
+  });
 };
 
 function replaceVerbs() {
+   $.get('verb.html',function(response){
+    var verbs = response.split('\n');
+    $('.verb').html(verbs[Math.floor ( Math.random() * verbs.length )]);
+  });
 };
 
 $(document).ready(function (){
-  // Code here
+  $('#random_noun').on('click',replaceNouns());
+  $('#random_verb').on('click', replaceVerbs());
 });
