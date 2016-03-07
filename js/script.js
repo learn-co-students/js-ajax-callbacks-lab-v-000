ba203d97022cc7cc9da6badae602d041d1be8f88
Justin Thomas <jthomas0209@gmail.com>
2016-03-07 08:31:41 -0500

    Done

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
 function replaceNouns() {
   $.get('noun.html',function(response){
-    var nouns = response.split('\n');
-    $('.noun').html(nouns[Math.floor ( Math.random() * nouns.length )]);
+    var nouns = response.trim().split('\n');
+    $('.noun').each(function(){
+      $(this).html(nouns[Math.floor ( Math.random() * nouns.length )]);
+    });    
+  }).fail(function(error){
+    alert("Function Failed: " + error.statusText);
   });
 };
 
 function replaceVerbs() {
-   $.get('verb.html',function(response){
-    var verbs = response.split('\n');
-    $('.verb').html(verbs[Math.floor ( Math.random() * verbs.length )]);
+  $.get('verb.html',function(response){
+    var verbs = response.trim().split('\n');
+    $('.verb').each(function(){
+      $(this).html(verbs[Math.floor ( Math.random() * verbs.length )]);
+    });
+  }).fail(function(error){
+    alert("Function Failed: " + error.statusText);
   });
 };
 
 $(document).ready(function (){
-  $('#random_noun').on('click',replaceNouns());
-  $('#random_verb').on('click', replaceVerbs());
+  $('#random_noun').on('click', replaceNouns);
+  $('#random_verb').on('click', replaceVerbs);
 });
