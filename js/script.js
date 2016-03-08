4af77d714b32cdc28e94db374909a41a6f8159bd
Kevin McCormack <wittyman37@yahoo.com>
2016-03-08 09:22:03 -0500

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
 function replaceNouns() {
+  $('#random_noun').on('click', function(event) {
+    $.get("noun.html", function(response) {
+      var nouns = response.trim().split("\n");
+
+      $(".noun").each(function(index, value) {
+        $(this).text(nouns[Math.floor(Math.random() * words.length)]);
+      });
+    });
+
+    event.preventDefault();
+  });
 };
 
 function replaceVerbs() {
+  $('#random_verb').on('click', function(event) {
+    $.get("verb.html", function(response) {
+      var verbs = response.trim().split("\n");
+
+      $(".verb").each(function(index, value) {
+        $(this).text(verbs[Math.floor(Math.random() * words.length)]);
+      });
+    });
+
+    event.preventDefault();
+  });
 };
 
 $(document).ready(function (){
   // Code here
+  replaceNouns();
+  replaceVerbs();
 });
