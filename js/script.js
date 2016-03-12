9bc1337f158407c1163385b5011858b731a90e44
dannyd4315 <dannyd4315@gmail.com>
2016-03-12 19:12:20 +1100

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
 function replaceNouns() {
+  $.get("noun.html", function(response){
+    // console.log(response);
+    var nouns = response.trim().split("\n")
+
+    $('.noun').each(function(){
+      var rand = nouns[Math.floor(Math.random() * nouns.length)];
+      $(this).html(rand);
+    }); 
+  }).fail(function(error){
+    alert("Something went wrong: " + error.statusText);
+  });
 };
 
 function replaceVerbs() {
+  $.get("verb.html", function(response){
+    // console.log(response);
+    var verbs = response.trim().split("\n")
+
+    $('.verb').each(function(){
+      var rand = verbs[Math.floor(Math.random() * verbs.length)];
+      $(this).html(rand);
+    }); 
+  }).fail(function(error){
+    alert("Something went wrong: " + error.statusText);
+  });
 };
 
 $(document).ready(function (){
-  // Code here
+  $('#random_noun').on('click', replaceNouns);
+  $('#random_verb').on('click', replaceVerbs);
 });
