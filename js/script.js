165a49469b1384ff30e9cf388250751cbf892595
pajamaw <pjpeterww@gmail.com>
2016-03-21 17:21:11 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
+$(document).ready(function(){
+  $('#random_noun').on('click', replaceNouns);
+  $('#random_verb').on('click', replaceVerbs);
+});
+
 function replaceNouns() {
-};
+ $.get('noun.html', function(data){
+  
+  var nouns = data.split("\n");
+
+    $('.noun').each(function(){
+      $(this).html(nouns[(Math.floor(Math.random() * (nouns.length)) + 0)
+])
+    })
+
+   }).fail(function(error){
+      alert('Error: '+error.statusText);
+    });;
+
+}
 
 function replaceVerbs() {
-};
+   $.get('verb.html', function(data){
+  
+  var verbs = data.split("\n");
 
-$(document).ready(function (){
-  // Code here
-});
+    $('.verb').each(function(){
+      $(this).html(verbs[(Math.floor(Math.random() * (verbs.length)) + 0)
+])
+    });
+
+   }).fail(function(error){
+      alert('Error: ' +error.statusText);;
+    });;
+
+}
+
+
