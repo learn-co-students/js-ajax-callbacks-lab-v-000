beec3144fa125b8366c20f29dde22874c33e5d21
alexbarron <alexbarron@gmail.com>
2016-03-28 19:16:59 -0700

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 function replaceNouns() {
+  $.get("noun.html",function(data) {
+    $('.noun').each(function() {
+      var words = data.split("\n");
+      var random = words[Math.floor(Math.random() * words.length)];
+      $(this).text(random);
+    });
+  }).fail(function(error) {
+    console.log("Something went wrong: " + error);
+  });
 };
 
 function replaceVerbs() {
+  $.get("verb.html",function(data) {
+    $('.verb').each(function() {
+      var words = data.split("\n");
+      var random = words[Math.floor(Math.random() * words.length)];
+      $(this).text(random);
+    });
+  }).fail(function(error) {
+    console.log("Something went wrong: " + error);
+  });
 };
 
 $(document).ready(function (){
-  // Code here
+  $('input#random_noun').click(replaceNouns);
+  $('input#random_verb').click(replaceVerbs);
 });
