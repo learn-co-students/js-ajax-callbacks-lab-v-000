6d877a493d9012963dc67027f5519298e4604554
Marco Tsui <marcotsui2003@yahoo.com>
2016-03-29 21:56:50 -0400

    done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
 function replaceNouns() {
-};
+  $.get('noun.html', function(data){
+    noun_list = data.split('\n');
+    noun_list.pop();
+    $('span.noun').each(function(index,item) {
+      $(this).html(noun_list[Math.floor(Math.random()*noun_list.length)])
+    });
+  }).fail(function(error) {
+    console.log("the error is:" + error);
+  })
+}
 
 function replaceVerbs() {
+  $.get('verb.html', function(data){
+    verb_list = data.split('\n');
+    verb_list.pop();
+    $('span.verb').each(function(index,item) {
+      $(this).html(verb_list[Math.floor(Math.random()*verb_list.length)])
+    });
+  });
 };
 
 $(document).ready(function (){
-  // Code here
+  $('#random_noun').on('click', replaceNouns);
+  $('#random_verb').on('click', replaceVerbs);
 });
