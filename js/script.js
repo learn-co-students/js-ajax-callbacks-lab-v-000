bc0282ea013edf88bd2052c674e0de22fe0e1613
c1505 <coreym247@gmail.com>
2016-04-03 22:28:47 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 function replaceNouns() {
+  $.get("words.html", function(response) {
+    $(".noun").each(function() {
+      $(this).html(random(response));
+    });
+  })
 };
 
 function replaceVerbs() {
+    $.get("words.html", function(response) {
+    $(".verb").each(function() {
+      $(this).html(random(response));
+    });
+  })
 };
+
+function random(string) {
+  var arr = string.split(",")
+  return arr[Math.floor(Math.random()*arr.length)]
+}
 
 $(document).ready(function (){
   // Code here
+  replaceNouns();
+  replaceVerbs();
+
 });
+
+
