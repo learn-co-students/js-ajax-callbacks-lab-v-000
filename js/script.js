ae63f56d1343a59f9d13e2fdcab7032592503f0d
pacosta613 <pedro.acosta613@gmail.com>
2016-04-07 15:48:24 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
 function replaceNouns() {
+  $.get('noun.html', function(data) {
+    var nouns = data.trim().split("\n");
+    var i = Math.floor(Math.random() * nouns.length);
+
+    $('.noun').each(function() {
+      $(this).html(nouns[i]);
+    });
+  }).fail(function(error) {
+    alert("Something went wrong:" + error);
+  });
 };
 
 function replaceVerbs() {
+  $.get('verb.html', function(data) {
+    var verbs = data.trim().split("\n");
+    var i = Math.floor(Math.random() * verbs.length);
+
+    $('.verb').each(function() {
+      $(this).html(verbs[i]);
+    });
+  }).fail(function(error) {
+    alert("Something went wrong:" + error);
+  });
 };
 
 $(document).ready(function (){
-  // Code here
+  $('input#random_noun').click(replaceNouns);
+  $('input#random_verb').click(replaceVerbs);
 });
