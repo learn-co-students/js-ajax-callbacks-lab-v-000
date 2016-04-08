c1bc222bfe12dc05d64421e2e151c533ae7ba6ad
NStephenson <nickstephenson90@gmail.com>
2016-04-08 14:36:40 -0400

    made all tests pass

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
 // function replaceNouns() {
 //   $.get("noun.html", function(response){
 //     $('.noun').each(function(index, noun){
-//       var randomNoun = response.split("\n")[Math.floor(Math.random() * 10)]
+//       var randomNoun = response.split("\n")[0]
 //       noun.textContent = randomNoun;
 //     });
+//   }).fail(function(error){
+//     alert("Something went wrong:" + error);
+//   });
+// };
+
+
+// function replaceVerbs() {
+//   $.get("verb.html", function(response){
+//     $('.verb').each(function(index, verb){
+//       var randomVerb = response.split("\n")[0]
+//       verb.textContent = randomVerb;
+//     });
+//   }).fail(function(error){
+//     alert("Something went wrong:" + error);
 //   });
 // };
 
 function replaceNouns() {
   $.get("noun.html", function(response){
-    var randomNoun = response.split("\n")[Math.floor(Math.random() * 10)];
-    $('.noun').html(randomNoun)
+    $('.noun').each(function(index, noun){
+      var randomNoun = response.split("\n")[Math.floor(Math.random() * 10)]
+      noun.textContent = randomNoun;
+    });
+  }).fail(function(error){
+    alert("Something went wrong:" + error);
   });
-}
+};
+
 
 function replaceVerbs() {
   $.get("verb.html", function(response){
@@ -20,12 +39,12 @@ function replaceVerbs() {
       var randomVerb = response.split("\n")[Math.floor(Math.random() * 10)]
       verb.textContent = randomVerb;
     });
+  }).fail(function(error){
+    alert("Something went wrong:" + error);
   });
 };
 
-// function randomFromResponse(response){
-//   return response.split("\n")[Math.floor(Math.random() * 10)];
-// }
+// Doc Ready
 
 $(document).ready(function (){
   $('#random_noun').on("click", function(){
