bc3a7e9e344d8231d306f0e099f776d12a5311fc
NStephenson <nickstephenson90@gmail.com>
2016-04-08 13:51:45 -0400

    working MadLibber

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
+// function replaceNouns() {
+//   $.get("noun.html", function(response){
+//     $('.noun').each(function(index, noun){
+//       var randomNoun = response.split("\n")[Math.floor(Math.random() * 10)]
+//       noun.textContent = randomNoun;
+//     });
+//   });
+// };
+
 function replaceNouns() {
-};
+  $.get("noun.html", function(response){
+    var randomNoun = response.split("\n")[Math.floor(Math.random() * 10)];
+    $('.noun').html(randomNoun)
+  });
+}
 
 function replaceVerbs() {
+  $.get("verb.html", function(response){
+    $('.verb').each(function(index, verb){
+      var randomVerb = response.split("\n")[Math.floor(Math.random() * 10)]
+      verb.textContent = randomVerb;
+    });
+  });
 };
 
+// function randomFromResponse(response){
+//   return response.split("\n")[Math.floor(Math.random() * 10)];
+// }
+
 $(document).ready(function (){
-  // Code here
+  $('#random_noun').on("click", function(){
+    replaceNouns();
+  });
+
+  $('#random_verb').on("click", function(){
+    replaceVerbs();
+  });
 });
