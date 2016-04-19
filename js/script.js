3db2c7ce1d08041c635e154ebe6826ea249fb21a
jjlippman <jjlippman@earthlink.net>
2016-04-19 11:56:49 -0600

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-function replaceNouns() {
-};
+// function replaceNouns() {
+//   .get("noun.html", function(response){
+
+//       $('.noun').html(response);
+//   });
+
+// }
 
 function replaceVerbs() {
-};
+  $('#random_verb').on('click', function(){
+      $.get("verb.html", function(response){
+          $('.verb').html(response);
+      });
+   });
+}
 
 $(document).ready(function (){
-  // Code here
+  replaceVerbs();
+  // replaceNouns();
+
 });
+
+
+ 
