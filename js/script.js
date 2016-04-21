ff24700822848788258d670769284227693e388e
jsgoldb <jgoldb@yahoo.com>
2016-04-21 18:22:34 -0500

    Done.

--- js/script.js
+++ js/script.js
@@ -1,32 +1,29 @@
-function replaceNouns() {
-  $('#random_noun').on('click', function(e){
-    $.get('noun.html', function(data){
-      var wordArray = data.split('\n');
-      $('.noun').each(function(index, noun){
-        $(this).text(wordArray[Math.floor(Math.random() * wordArray.length)]);
-      })
-      debugger;
-    }).fail(function(error){
-      alert('Something went wrong: ' + error);
-    })
+function replaceNouns(e) {
+  $.get('noun.html', function(data){
+    var wordArray = data.split('\n');
+    $('.noun').each(function(index, noun){
+      $(this).text(wordArray[Math.floor(Math.random() * wordArray.length)]);
+    });
+  }).fail(function(error){
+    alert('Something went wrong: ' + error);
   })
 };
 
-function replaceVerbs() {
-  $('#random_verb').on('click', function(e){
-    $.get('verb.html', function(data){
-      var wordArray = data.split('\n');
-      $('.verb').text(function(index, verb){
-        $(this).text(wordArray[Math.floor(Math.random() * wordArray.length)]);
-      })
-    }).fail(function(error){
-      alert('Something went wrong: ' + error);
-    })
+function replaceVerbs(e) {
+  $.get('verb.html', function(data){
+    var wordArray = data.split('\n');
+    $('.verb').each(function(index, verb){
+      $(this).text(wordArray[Math.floor(Math.random() * wordArray.length)]);
+    });
+  }).fail(function(error){
+    alert('Something went wrong: ' + error);
   })
 };
 
 $(document).ready(function (){
   // Code here
-  replaceNouns();
-  replaceVerbs();
+  $('#random_verb').on('click', replaceVerbs());
+  $('#random_noun').on('click', replaceNouns());
+
+
 });
