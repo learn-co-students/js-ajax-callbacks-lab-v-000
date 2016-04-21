f0094ba601a4a02d86de3cf36c27b459f2948cdb
jsgoldb <jgoldb@yahoo.com>
2016-04-21 17:38:29 -0500

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
 function replaceNouns() {
+  $('#random_noun').on('click', function(e){
+    $.get('noun.html', function(data){
+      var wordArray = data.split('\n');
+      $('.noun').each(function(index, noun){
+        $(this).text(wordArray[Math.floor(Math.random() * wordArray.length)]);
+      })
+      debugger;
+    }).fail(function(error){
+      alert('Something went wrong: ' + error);
+    })
+  })
 };
 
 function replaceVerbs() {
+  $('#random_verb').on('click', function(e){
+    $.get('verb.html', function(data){
+      var wordArray = data.split('\n');
+      $('.verb').text(function(index, verb){
+        $(this).text(wordArray[Math.floor(Math.random() * wordArray.length)]);
+      })
+    }).fail(function(error){
+      alert('Something went wrong: ' + error);
+    })
+  })
 };
 
 $(document).ready(function (){
   // Code here
+  replaceNouns();
+  replaceVerbs();
 });
