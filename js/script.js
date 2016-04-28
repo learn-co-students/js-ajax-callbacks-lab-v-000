6e46dff790335feefe5dbdf84818bb7d32655dde
authorbeard <msnolan@gmail.com>
2016-04-28 00:18:36 -0400

    refactored; still works, specs still fail

--- js/script.js
+++ js/script.js
@@ -1,34 +1,43 @@
+var nouns;
+var verbs;
+
 function replaceNouns() {
   $('#random_noun').click(function() {
-    $.get("html/noun.html", function(data){
-        var nouns = data.split("\n")
-    debugger;
-        $('.noun').each(function(index, noun) {
-          $(this).text(nouns[Math.floor(Math.random() * nouns.length)])
+    $('.noun').map(function(noun, index) {
+       $(this).text(nouns[Math.floor(Math.random() * nouns.length)])
         })
-    }).fail(function(error) {
-      $('div').append(error['responseText'])
     })
-  })
-  event.preventDefault()    
 };
 
 function replaceVerbs() {
   $('#random_verb').click(function() {
-  $.get("html/verb.html", function(data){
-      var verbs = data.split("\n")
-  debugger;
-      $('.verb').each(function(index, verb) {
-        $(this).text(verbs[Math.floor(Math.random() * verbs.length)])
+    $('.verb').each(function(index, verb) {
+      $(this).text(verbs[Math.floor(Math.random() * verbs.length)])
       })
-  }).fail(function(error) {
-    $('div').append(error['responseText'])
-    })
   })
 };
 
+
 $(document).ready(function (){
   
+
+  $.get("html/noun.html")
+    .done(function(data){
+      nouns = data.trim().split("\n")
+    })
+    .fail(function(error) {
+      $('div').append(error['responseText'])
+    })
+
+  $.get("html/verb.html")
+    .done(function(data){
+      verbs = data.trim().split("\n")
+    })
+    .fail(function(error) {
+      $('div').append(error['responseText'])
+    })
+
+
   replaceNouns();
   replaceVerbs();
 });
