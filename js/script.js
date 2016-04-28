cad3c4be40bad03443a1f1b11b793336c09a1d90
authorbeard <msnolan@gmail.com>
2016-04-27 23:50:39 -0400

    page functions correctly, specs failing

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
 function replaceNouns() {
+  $('#random_noun').click(function() {
+    $.get("html/noun.html", function(data){
+        var nouns = data.split("\n")
+    debugger;
+        $('.noun').each(function(index, noun) {
+          $(this).text(nouns[Math.floor(Math.random() * nouns.length)])
+        })
+    }).fail(function(error) {
+      $('div').append(error['responseText'])
+    })
+  })
+  event.preventDefault()    
 };
 
 function replaceVerbs() {
+  $('#random_verb').click(function() {
+  $.get("html/verb.html", function(data){
+      var verbs = data.split("\n")
+  debugger;
+      $('.verb').each(function(index, verb) {
+        $(this).text(verbs[Math.floor(Math.random() * verbs.length)])
+      })
+  }).fail(function(error) {
+    $('div').append(error['responseText'])
+    })
+  })
 };
 
 $(document).ready(function (){
-  // Code here
+  
+  replaceNouns();
+  replaceVerbs();
 });
