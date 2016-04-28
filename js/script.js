73c970311e048de5f8627f0210205228b1953bb6
franco84 <franco84@optonline.net>
2016-04-28 13:14:26 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 function replaceNouns() {
-};
+  $.get('noun.html', function(nouns){
+    var nounArray = nouns.split(/\n/).filter(function(i){return i !==''});
+  $('.noun').each(function(){
+    var randomNoun = Math.floor(Math.random() * nounArray.length);
+    $(this).html(nounArray[randomNoun]);
+    });
+  }).fail(function(error) {
+    console.log("There was an error: " + error);
+  });
+}
 
 function replaceVerbs() {
+  $.get('verb.html', function(verbs){
+  var verbArray = verbs.split(/\n/).filter(function(i){return i !==''});
+    $('.verb').each(function(){
+      var randomVerb = Math.floor(Math.random() * verbArray.length);
+      $(this).html(verbArray[randomVerb]);
+    });
+  }).fail(function(error) {
+    console.log("There was an error: " + error);
+  });
 };
 
-$(document).ready(function (){
-  // Code here
+$(document).ready(function() {
+    $('#random_noun').click(replaceNouns);
+    $('#random_verb').click(replaceVerbs);
 });
