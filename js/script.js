87dd6d30ab33f64d4b314a701abff30159196e3e
shinyrachel <rachel.kory@gmail.com>
2016-04-29 10:58:16 -0400

    complete

--- js/script.js
+++ js/script.js
@@ -1,9 +1,42 @@
 function replaceNouns() {
+    $.get("noun.html", function(response){
+      var words = response.split("\n");
+    
+      $('.noun').each(function(index, element){
+        var num = Math.floor(Math.random() * words.length);
+        var noun = words[num];
+        $(this).html(noun);    
+      });
+    }).fail(function(error){
+      alert("Something's wrong: " + error);
+    });
 };
 
 function replaceVerbs() {
+  $.get("verb.html", function(response){
+    var words = response.split("\n");
+    
+    $('.verb').each(function(index, element){
+      var num = Math.floor(Math.random() * words.length);
+      var verb = words[num];
+      $(this).html(verb);    
+    });
+  }).fail(function(error){
+      alert("Something's wrong: " + error);
+  });
 };
+
+function handleNounClick() {
+  $('#random_noun').on("click", replaceNouns);
+}
+
+function handleVerbClick() {
+  $('#random_verb').on("click", replaceVerbs);
+}
+
 
 $(document).ready(function (){
   // Code here
+  handleNounClick();
+  handleVerbClick();
 });
