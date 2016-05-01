de8716fcbd0b10f1a8df20cec2af904a665c6bae
Dakota Martinez <dakotaleemusic@gmail.com>
2016-05-01 07:58:08 +0000

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 function replaceNouns() {
+  $.get('noun.html', function(data) {
+    var nouns = data.split("\n").filter(v=>v!='');
+    sorted = nouns.sort(function() { return 0.5 - Math.random() });
+    $('.noun').each(function(index) {
+      $(this).text(sorted[index]);
+    });
+  });
 };
 
 function replaceVerbs() {
+  $.get('verb.html', function(data) {
+    var verbs = data.split("\n").filter(v=>v!='');
+    sorted = verbs.sort(function() { return 0.5 - Math.random() });
+    $('.verb').each(function(index) {
+      $(this).text(sorted[index]);
+    });
+  });
 };
 
 $(document).ready(function (){
-  // Code here
+  $('#random_noun').on("click", replaceNouns);
+  $('#random_verb').on("click", replaceVerbs);
 });
