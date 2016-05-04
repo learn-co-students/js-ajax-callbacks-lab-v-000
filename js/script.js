69c2bb613ec6a6e5a3050b1673c76f29a1103896
the-widget <brennenawana108ny@gmail.com>
2016-05-03 20:01:03 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
+var num = Math.floor(Math.random() * 10 + 1)
+
 function replaceNouns() {
+  $.get('noun.html', function(response){
+    var array = response.split("\n");
+    $('.noun').each(function(){
+      $(this).html(array[num]);
+    });
+  }).fail(function(error) {
+    alert('The request failed: ' + error.statusText);
+  });
 };
+
 
 function replaceVerbs() {
-};
+  $.get('verb.html', function(response){
+    var array = response.split("\n");
+    $('.verb').each(function(){
+      $(this).html(array[num]);
+    });
+  }).fail(function(error) {
+    alert('The request failed: ' + error.statusText);
+  });
+}
 
 $(document).ready(function (){
-  // Code here
+  
+  $('#random_noun').click(replaceNouns);
+  $('#random_verb').click(replaceVerbs);
+
 });
+
