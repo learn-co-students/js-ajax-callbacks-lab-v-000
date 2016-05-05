4430256356b48f5e6a9d65efb93ff20a7079bf1e
Brian Holland <beehollander@gmail.com>
2016-05-05 15:29:14 -0500

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
+var nouns, verbs;
 function replaceNouns() {
+  $.get('noun.html', function(response){
+    nouns = response.split('\n');
+    nouns.pop();
+    $('.noun').each(function(){
+      $(this).text(nouns[Math.floor(Math.random()*nouns.length)]);
+    });
+  });
 };
 
 function replaceVerbs() {
+  $.get('verb.html', function(response){
+    verbs = response.split('\n');
+    verbs.pop();
+    $('.verb').each(function(){
+      $(this).text(verbs[Math.floor(Math.random()*verbs.length)]);
+    });
+  });
 };
 
 $(document).ready(function (){
   // Code here
+  $('#random_noun').on('click', replaceNouns);
+  $('#random_verb').on('click', replaceVerbs);
 });
