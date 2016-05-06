254dd836e9e151365a875b0346a25740d969c0d1
Smulligan85 <sean.mulligan85@gmail.com>
2016-05-05 18:29:31 -0700

    Add ajax request functions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 function replaceNouns() {
-};
+     $.get('noun.html', function(response) {
+       var nouns = response.split("\n");
+       $('.noun').each(function() {
+          var randomNum = Math.floor(Math.random() * nouns.length);
+          $(this).html(nouns[randomNum]);
+       });
+    }).fail(function(error) {
+      alert("Something went wrong " + error);
+    });
+}
 
 function replaceVerbs() {
-};
+    $.get('verb.html', function(response) {
+       var verbs = response.split("\n");
+       $('.verb').each(function() {
+          var randomNum = Math.floor(Math.random() * verbs.length);
+          $(this).html(verbs[randomNum]);
+       });
+    }).fail(function(error) {
+      alert("Something went wrong " + error);
+    });
+}
 
 $(document).ready(function (){
   // Code here
+  $('#random_noun').on('click', replaceNouns);
+  $('#random_verb').on('click', replaceVerbs);
 });
