78c5ec295b0fe9dbc67fef8df47e019130c78d14
cernanb <cernanb@gmail.com>
2016-05-05 22:48:34 -0700

    used ajax get method to retrieve values from url

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
 function replaceNouns() {
+  $.get('words.html', function(data){
+    var words = data.trim().split('\n');
+    // words = words.pop();
+    var wordToEnter = words[Math.floor(Math.random() * words.length)];
+    $('.noun').html(wordToEnter);
+  }).fail(function(error){
+    alert('something went wrong');
+  });
 };
 
 function replaceVerbs() {
+  $.get('words.html', function(data){
+    var words = data.trim().split('\n');
+    var wordToEnter = words[Math.floor(Math.random() * words.length)]
+    $('.verb').html(wordToEnter);
+  }).fail(function(error){
+    alert('something went wrong');
+  });
 };
 
-$(document).ready(function (){
-  // Code here
+$(document).ready(function(){
+
+  $('#random_noun').on('click', replaceNouns);
+  $('#random_verb').on('click', replaceVerbs);
 });
