838775cf9dec19ea2ab50485305f2318d8f22cb2
Robert <robmcarrier@gmail.com>
2016-05-07 16:08:42 -0700

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
 function replaceNouns() {
+  $.get('noun.html', function(data){
+    var noun = data.trim().split("\n");
+    $('.noun').each(function(){
+      var random = Math.floor(Math.random() * noun.length);
+      var random_noun = noun[random];
+      $(this).html(random_noun);
+    })
+  }).fail(function(error){
+    alert('The request failed: ' + error.statusText);
+  });
 };
 
 function replaceVerbs() {
+  $.get('verb.html', function(data){
+    var verb = data.trim().split("\n");
+    $('.verb').each(function(){
+      var random = Math.floor(Math.random() * verb.length);
+      var random_verb = verb[random];
+      $(this).html(random_verb);
+    })
+  }).fail(function(error){
+    alert('The request failed: ' + error.statusText);
+  });
 };
 
 $(document).ready(function (){
+  $('#random_noun').click(replaceNouns);
   // Code here
+  $('#random_verb').click(replaceVerbs);
 });
