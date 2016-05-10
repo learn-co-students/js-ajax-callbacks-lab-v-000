19326227bcd9270271e8575cb7ec5124de2736fd
evanscloud <dijkstrascloud@gmail.com>
2016-05-10 12:32:34 -0400

    Use ajax requests and callbacks to render data

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
 function replaceNouns() {
+  $.get('noun.html', function(data){
+    var words = data.split('\n');
+    $('.noun').each(function(){
+      var randNum = Math.floor(Math.random() * words.length);
+      var response = words[randNum];
+      $(this).html(response);
+    });
+  }).fail(function(error){
+    console.log('Something went wrong: ' + error);
+  });
 };
 
 function replaceVerbs() {
+  $.get('verb.html', function(data){
+    var words = data.split('\n');
+    $('.verb').each(function(){
+      var randNum = Math.floor(Math.random() * words.length);
+      var response = words[randNum]
+      $(this).html(response);
+    });
+  }).fail(function(error){
+    console.log('Something went wrong: ' + error);
+  });
 };
 
 $(document).ready(function (){
-  // Code here
+  $('#random_noun').on('click', replaceNouns());
+  $('#random_verb').on('click', replaceVerbs());
 });
