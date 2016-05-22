4c8b8906d2c30488e92376d7f1d31914acefc461
Tyler Smith <oats000@hotmail.com>
2016-05-22 11:00:46 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
+//Note: This script currently works as described. Not sure why the test specs are fail but I'll return later to fix
+
 function replaceNouns() {
   $('#random_noun').on('click', function () {
     $.get("noun.html", function(data) {
-      var nounArray = data.split(/\n/);
+      var nounArray = data.trim().split(/\n/);
       console.log(Math.floor(Math.random() * nounArray.length));
       $(".noun").each(function(index, noun) {
         //randomly selects from the noun list and adds to each .noun class
-        $(this).text(nounArray[Math.round(Math.random() * (nounArray.length - 1))]);
+        $(this).html(nounArray[Math.round(Math.random() * (nounArray.length - 1))]);
       });
     }).fail(function(error) {
       // This is called when an error occurs
-      $("#errors").append('Something went wrong: ' + error);
+      alert('Something went wrong: ' + error.statusText);
     });
   })
 };
 
 function replaceVerbs() {
   $('#random_verb').on('click', function () {
-    $.get("verb.html", function(data) {
-      var verbArray = data.split(/\n/);
+    $.get("vrb.html", function(data) {
+      var verbArray = data.trim().split(/\n/);
       console.log(verbArray);
       $(".verb").each(function(index, noun) {
         //randomly selects from the noun list and adds to each .noun class
-        $(this).text(verbArray[Math.round(Math.random() * (verbArray.length - 1))]);
+        $(this).html(verbArray[Math.round(Math.random() * (verbArray.length - 1))]);
       });
     }).fail(function(error) {
       // This is called when an error occurs
-      $("#errors").append('Something went wrong: ' + error);
+      alert('Something went wrong: ' + error.statusText);
     });
   })
 };
