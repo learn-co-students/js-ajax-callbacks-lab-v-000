99d52ad259725bc9f3edb1a502ae5b22b4fd3db4
agdavid <antonio.g.david@gmail.com>
2016-05-24 12:43:13 -0400

    working random_noun generator

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 function replaceNouns() {
+  $('#random_noun').on('click', function(e) {  
+    $.get("noun.html", function(response) {
+      //turn into array ["store", "street"...]
+      var nouns = response.split(/\s/);
+      //iterate over each .noun in the DOM
+      $('.noun').each(function(index, noun) {
+        //insert a random noun
+        var randomNoun = nouns[Math.floor(Math.random()*nouns.length)];
+        $(this).append(randomNoun);
+      });
+    });
+  });
 };
 
 function replaceVerbs() {
@@ -6,4 +18,5 @@ function replaceVerbs() {
 
 $(document).ready(function (){
   // Code here
+  replaceNouns();
 });
