f91a8f33ae5f14656b07e6a432e8cd4564462843
agdavid <antonio.g.david@gmail.com>
2016-05-24 12:48:21 -0400

    working random_verb generator

--- js/script.js
+++ js/script.js
@@ -8,15 +8,30 @@ function replaceNouns() {
         //insert a random noun
         var randomNoun = nouns[Math.floor(Math.random()*nouns.length)];
         $(this).append(randomNoun);
-      });
-    });
-  });
+      }); // .each
+    })
+    ; // .get
+  }); //. on
 };
 
 function replaceVerbs() {
+  $('#random_verb').on('click', function(e) {  
+    $.get("verb.html", function(response) {
+      //turn into array ["store", "street"...]
+      var verbs = response.split(/\s/);
+      //iterate over each .noun in the DOM
+      $('.verb').each(function(index, verb) {
+        //insert a random noun
+        var randomVerb = verbs[Math.floor(Math.random()*verbs.length)];
+        $(this).append(randomVerb);
+      }); // .each
+    })
+    ; // .get
+  }); //. on
 };
 
 $(document).ready(function (){
   // Code here
   replaceNouns();
+  replaceVerbs();
 });
