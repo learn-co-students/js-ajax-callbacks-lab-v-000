49a980da5d0ffd2847ffda53261c498c99636b48
agdavid <antonio.g.david@gmail.com>
2016-05-24 12:55:34 -0400

    working errors to console.log

--- js/script.js
+++ js/script.js
@@ -1,32 +1,34 @@
 function replaceNouns() {
   $('#random_noun').on('click', function(e) {  
     $.get("noun.html", function(response) {
-      //turn into array ["store", "street"...]
-      var nouns = response.split(/\s/);
+      //turn into array ["store", "street"...] and remove empties ""
+      var nouns = response.split(/\s/).filter(function(word){return word!==""});
       //iterate over each .noun in the DOM
       $('.noun').each(function(index, noun) {
         //insert a random noun
         var randomNoun = nouns[Math.floor(Math.random()*nouns.length)];
         $(this).append(randomNoun);
       }); // .each
-    })
-    ; // .get
+    }).fail(function(error) {
+      console.log('Something went wrong:' + error);
+    }); // .get .fail
   }); //. on
 };
 
 function replaceVerbs() {
   $('#random_verb').on('click', function(e) {  
     $.get("verb.html", function(response) {
-      //turn into array ["store", "street"...]
-      var verbs = response.split(/\s/);
+      //turn into array ["store", "street"...] and remove empties ""
+      var verbs = response.split(/\s/).filter(function(word){return word!==""});
       //iterate over each .noun in the DOM
       $('.verb').each(function(index, verb) {
         //insert a random noun
         var randomVerb = verbs[Math.floor(Math.random()*verbs.length)];
         $(this).append(randomVerb);
       }); // .each
-    })
-    ; // .get
+    }).fail(function(error) {
+      console.log('Something went wrong:' + error);
+    }); // .get .fail
   }); //. on
 };
 
