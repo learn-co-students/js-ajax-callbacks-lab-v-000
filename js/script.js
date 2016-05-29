58a63c5be7a8d2ac858c1c1775a4cbeda4b8f3f9
katherinethai <kthai1618@gmail.com>
2016-05-28 23:32:57 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
 function replaceNouns() {
+  $.get('noun.html', function (response) {
+    var nouns = response.trim().split("\n");
+
+    $('.noun').each(function() {
+      var random_index = Math.floor(Math.random() * nouns.length)
+      var noun = nouns[random_index];
+      $(this).html(noun);
+    });
+  }).fail(function (error){
+    alert('The request failed: ' + error.statusText);
+  });
 };
 
 function replaceVerbs() {
+  $.get('verb.html', function (response) {
+    var verbs = response.trim().split("\n");
+
+    $('.verb').each(function() {
+      var random_index = Math.floor(Math.random() * verbs.length)
+      var verb = verbs[random_index];
+      $(this).html(verb);
+    });
+  }).fail(function (error){
+    alert('The request failed: ' + error.statusText);
+  });
 };
 
 $(document).ready(function (){
-  // Code here
+  $('#random_noun').click(replaceNouns);
+  $('#random_verb').click(replaceVerbs);
 });
