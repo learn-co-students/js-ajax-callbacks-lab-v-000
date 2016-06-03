9b2efe444d3fd96d1244c857ff03c7363499b114
Seth Lachman <lachmanseth17@gmail.com>
2016-06-03 16:16:53 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
 function replaceNouns() {
+     $.get('noun.html', function (data){
+
+      var noun = data.trim().split("\n");
+      $('.noun').each(function(){
+        var randomizeNouns = Math.floor(Math.random() * noun.length);
+        $(this).html(noun[randomizeNouns]);
+      });
+    }).fail(function (error){
+      alert('The request has failed: ' + error.statusText);
+    });
 };
 
 function replaceVerbs() {
+  $.get('verb.html', function(data) {
+       var verb = data.trim().split("\n");
+
+       $('.verb').each(function(){ 
+          var randomizeVerbs = Math.floor(Math.random() * verb.length);
+          $(this).html(verb[randomizeVerbs]);
+       });
+  }).fail(function (error) { 
+      alert('The request has failed: ' + error.statusText);
+  });
 };
 
 $(document).ready(function (){
   // Code here
+  $('#random_noun').click(replaceNouns);
+  $('#random_verb').click(replaceVerbs);
 });
