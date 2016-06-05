6fde4d8080c2de211b04b1b0789bc8e7d02b5c7e
gormanjp <jpgorman8@gmail.com>
2016-06-04 20:29:40 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 function replaceNouns() {
+		$.get("noun.html", function(response) {
+			var noun = response.split("\n")
+			var nounSelect = $(".noun")
+			$(nounSelect[0]).text(noun[Math.floor(Math.random() * noun.length)]);
+			$(nounSelect[1]).text(noun[Math.floor(Math.random() * noun.length)]);
+	})
 };
 
 function replaceVerbs() {
+		$.get("verb.html", function(response) {
+			var noun = response.split("\n")
+			var randomNoun = noun[Math.floor(Math.random() * noun.length)]
+			var verbSelect = $(".verb");
+			$(verbSelect[0]).text(noun[Math.floor(Math.random() * noun.length)]);
+			$(verbSelect[1]).text(noun[Math.floor(Math.random() * noun.length)]);
+			$(verbSelect[2]).text(noun[Math.floor(Math.random() * noun.length)]);
+
+	})
 };
 
 $(document).ready(function (){
-  // Code here
+  $("#random_noun").click(replaceNouns);
+  $("#random_verb").click(replaceVerbs);
 });
