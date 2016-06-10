3981591263651ba32b1f1f24edb437a2770d5b13
John Carlo Malto <jakemalto2014@gmail.com>
2016-06-10 05:27:34 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,47 @@
+function randomItem(array){
+  var i = Math.floor((Math.random() * array.length));
+  return array[i];
+}
+
 function replaceNouns() {
-};
+  $('input:first').click(function(event){
+  $.get("noun.html", function(response){
+    var content = response;
+    var noun_array = content.trim().split("\n");
+    noun_array.filter(Boolean);
+    $(".noun").each(function(index, noun){
+      $(this).html(randomItem(noun_array));
+    });
+ }).fail(function(error){
+   alert('Something went wrong: ' + error);
+ })
+});
+}
 
 function replaceVerbs() {
-};
+  $('input:last').click(function(event){
+  $.get("verb.html", function(response){
+    var content = response;
+    var noun_array = content.trim().split("\n");
+    noun_array.filter(Boolean);
+    $(".verb").each(function(index, noun){
+      $(this).html(randomItem(noun_array));
+    });
+ }).fail(function(error){
+   alert('Something went wrong: ' + error);
+ })
+});
+}
+
+
+
 
 $(document).ready(function (){
   // Code here
+  replaceNouns();
+  replaceVerbs();
+
+
+
+
 });
