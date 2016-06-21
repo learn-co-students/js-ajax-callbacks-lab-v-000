6729a0a2444a53697715a95dc8aa6c0570220dbd
Christopher Bruen <bruen.chris@gmail.com>
2016-06-21 00:15:04 +0000

    Done.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 function replaceNouns() {
+  $.get("/noun.html", function(data) {
+    var nouns = data.trim().split("\n");
+    $('.noun').each(function() {
+      var index = Math.floor(Math.random()*nouns.length);
+      $(this).html(nouns[index]);
+    });
+  }).fail(function(error) {
+    alert('The request failed: ' + error.statusText);
+  });
 };
 
 function replaceVerbs() {
+  $.get('/verb.html', function(data) {
+    var verbs = data.trim().split("\n");
+    $('.verb').each(function() {
+      var index = Math.floor(Math.random() * verbs.length);
+      $(this).html(verbs[index]);
+    });
+  }).fail(function(error) {
+    alert("The request failed: " + error.statusText);
+  });
 };
 
 $(document).ready(function (){
