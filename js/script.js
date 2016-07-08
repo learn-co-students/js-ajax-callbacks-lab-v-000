ca8b61b91e44088003689b1d1f3a0642db9d304b
cassaram09 <cassaram09@gmail.com>
2016-07-07 23:49:15 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 function replaceNouns() {
+  $.get('../noun.html', function(response){
+    var nouns = response.split("\n").slice(0, -1);
+    $(".noun").each(function(index, span){
+      $(this).text(nouns[Math.floor(Math.random()*nouns.length)])
+    })
+  }).fail(function(error){
+    alert("There was an error retreiving nouns.");
+  });
 };
 
 function replaceVerbs() {
+  $.get('../verb.html', function(response){
+    var verbs = response.split("\n").slice(0, -1);
+    $(".verb").each(function(index, span){
+      $(this).text(verbs[Math.floor(Math.random()*verbs.length)])
+    })
+  })
 };
 
 $(document).ready(function (){
-  // Code here
+  $("#random_noun").on("click", replaceNouns );
+  $("#random_verb").on("click", replaceVerbs );
 });
