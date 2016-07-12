665bdc55e9483461782f9f62574d4ad2618fe91f
Peter Jacobson <github email address>
2016-07-12 14:56:08 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 function replaceNouns() {
+    $.get("noun.html", function(response) {
+      var nouns = response.trim().split("\n");
+      $(".noun").each(function() {
+        var randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+        $(this).html(randomNoun);
+      })
+    }).fail(function(error) {
+      alert('The request failed: ' + error.statusText);
+    })
 };
 
 function replaceVerbs() {
+    $.get("verb.html", function(response) {
+      var verbs = response.trim().split("\n");
+      $(".verb").each(function() {
+        var randomVerb = verbs[Math.floor(Math.random() * verbs.length)];
+        $(this).html(randomVerb);
+      })
+    }).fail(function(error) {
+      alert('The request failed: ' + error.statusText);
+    })
 };
-
-$(document).ready(function (){
-  // Code here
-});
