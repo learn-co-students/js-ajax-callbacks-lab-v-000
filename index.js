55eb02f2ea0cc8b7af68815d41f94ea924697cb0
Savannah Scott <sascott624@gmail.com>
2016-07-19 17:48:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
+  // #user-details-partial is the partial template
 }
 
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+function searchRepositories(){
+  var search = $('#searchTerms').val();
+  $.get("https://api.github.com/search/repositories?q=" + search, function(response){
+    $('#results').html(response);
+  })
+}
