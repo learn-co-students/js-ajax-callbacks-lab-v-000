ac147880871d571e6212c32bcdc865a3d48d7689
Jason Casares <jasonc@csdrafting.com>
2016-07-29 17:51:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
-}
+};
 
 $(document).ready(function (){
   handlebarsSetup()
