786d6631b234bf125d45b30ad14ad9c889059f2e
glee38 <glee3388@gmail.com>
2016-08-18 19:28:50 -0400

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ function handlebarsSetup() {
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
 
+function searchRepositories() {
+    const searchTerms = $("#searchTerms").val();
+    const baseUrl = "https://api.github.com/search/repositories?q="
+    const url = baseUrl + searchTerms
+
+    $.get(url, function(data) {
+      const template = Handlebars.compile($("#results-template").html());
+      $('#results').html(template(data));
+    }).fail(displayError());
+}
+
+function displayError() {
+  $('#errors').html("Oops! There's been an error. Please try again.")
+}
+
 $(document).ready(function (){
   handlebarsSetup()
 });
