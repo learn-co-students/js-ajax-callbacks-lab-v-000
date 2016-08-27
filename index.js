80062b99e7f9d716e754c8f4f8cb898d75725ffa
Jason <jasonkwong11@gmail.com>
2016-08-26 20:04:48 -0400

    done

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
+$(document).ready(function (){
+  handlebarsSetup()
+  $('#search').on('click', searchRepositories);
+
+});
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
 
-$(document).ready(function (){
-  handlebarsSetup()
-});
+function searchRepositories(event) {
+  var search = $('#searchTerms').val()
+  var url = 'https://api.github.com/search/repositories?q=' + search
+  $.get(url, data => {$('#results').html(Handlebars.compile($('#results-template').html())(data))})
+    .fail(error => {displayError()})
+}
+
+function showCommits(repoLink){
+  var user = repoLink.dataset.owner
+  var repo = repoLink.dataset.repository
+  $.get(`https://api.github.com/repos/${user}/${repo}/commits`, data => {
+    const template = Handlebars.compile($('#commits-template').html())
+    $('#details').html(template(data))
+  }).fail(error =>{
+    displayError()
+  })
+}
+
+function displayError(){
+  $('#errors').html("There's been an error. Please try again.");
+}
