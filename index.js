fb59b0fe6f45c7b183569aa087130ab1f41e8b72
Devin Burnette <devin.burnette@gmail.com>
2016-08-28 20:55:27 +0000

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,38 @@ function handlebarsSetup() {
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
 
+function searchRepositories() {
+  var searchTerms = $('#searchTerms').val();
+  var url = 'https://api.github.com/search/repositories?q=' + searchTerms;
+  $.get(url, function(data) {
+    var template = Handlebars.compile($('#results-template').html());
+    $('#results').html(template(data));
+  }).fail(function(error) {
+    displayError(error);
+  });
+}
+
+function showCommits(obj) {
+  var owner = obj.dataset.owner;
+  var repo = obj.dataset.repository;
+  var url = 'https://api.github.com/repos/'+ owner + '/' + repo + '/commits';
+  $.get(url, function(data) {
+    var template = Handlebars.compile($('#commits-template').html());
+    $('#details').html(template(data));
+  }).fail(function(error) {
+    displayError(error);
+  });
+}
+
+function displayError(error) {
+  $('#errors').html("<p>I'm sorry, there's been an error. Please try again.</p>");
+}
+
+function handlebarsSetup() {
+  //put any handlebars setup in here
+  Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
+}
+
 $(document).ready(function (){
-  handlebarsSetup()
+  handlebarsSetup();
 });
