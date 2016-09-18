3e21fa96b6acc4b19f158bc9d05cf50df5567ccc
Nathaniel Miller <dukeoflaser@gmail.com>
2016-09-18 13:29:18 -0700

    Done.

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function showCommits(x){
   var repo = x['attributes']['repo-name']['value']
   var owner = x['attributes']['owner']['value']
 
+  debugger;
   var url = `https://api.github.com/repos/${owner}/${repo}/commits`
 
   $.get(url, function(response){
