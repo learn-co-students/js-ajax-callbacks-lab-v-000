1e0fb8ff4c060790c21db0b9a09f932d61aa85ac
Nathaniel Miller <dukeoflaser@gmail.com>
2016-09-18 13:15:38 -0700

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
+$(document).ready(function (){
+  handlebarsSetup();
+
+});
+
+
 function handlebarsSetup() {
-  //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
 
-$(document).ready(function (){
-  handlebarsSetup()
-});
+function displayError(){
+  $('#errors').append("I'm sorry, there's been an error. Please try again.");
+}
+
+function showCommits(x){
+  var repo = x['attributes']['repo-name']['value']
+  var owner = x['attributes']['owner']['value']
+
+  var url = `https://api.github.com/repos/${owner}/${repo}/commits`
+
+  $.get(url, function(response){
+    displayCommits(response);
+  }).fail(function(){
+    displayError();
+  });
+}
+
+function searchRepositories(){
+
+  var searchTerms = $('#searchTerms').val();
+  var url = "https://api.github.com/search/repositories?q=" + searchTerms + "&sort=stars&order=desc"
+
+  $.get(url, function(response){
+    displayResults(response);
+  }).fail(function(){
+    displayError();
+  });
+}
+
+function displayResults(response){
+  console.log(response)
+  var template = $('#results-template').html();
+  var compiled_template = Handlebars.compile(template);
+
+  $('#results').html(compiled_template(response));
+
+}
+
+function displayCommits(response){
+  console.log(response)
+  var template = $('#commits-template').html();
+  var compiled_template = Handlebars.compile(template);
+
+  $('#details').html(compiled_template(response));
+}
