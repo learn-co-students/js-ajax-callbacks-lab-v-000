abd9dc2c9d78db85eeba044ac432ea555ffe98d2
kentleong <kentleong@github.com>
2016-09-22 19:41:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function searchRepositories() {
+  var searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data) {
+      var template = Handlebars.compile($('#results-template').html())
+      $('#results').html(template(data))
+    }).fail(function() {
+      displayError()
+    })
+}
+function showCommits(el) {
+  var owner = el.dataset.owner
+  var repo = el.dataset.repository
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, function(data) {
+    var template = Handlebars.compile($('#commits-template').html())
+    $('#details').html(template(data))
+  }).fail(function(error) {
+    displayError()
+  })
+}
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
