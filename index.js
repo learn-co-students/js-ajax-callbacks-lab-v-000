7fe905c3f7095d759d9fb1ceb388a3631a054da8
gabe <gj+code@mail.co.de>
2016-09-25 23:47:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 function handlebarsSetup() {
   //put any handlebars setup in here
-  Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
+  Handlebars.registerPartial("userDetails", $("#user-details-partial").html());
 }
 
+function searchRepositories() {
+  var searchTerms = $('#searchTerms').val();
+  var searchURL = `https://api.github.com/search/repositories?q=${searchTerms}`
+  $.get(searchURL, function(data) {
+    var template = Handlebars.compile($('#results-handlebars-template').html());
+    $('#results').html(template(data));
+  }).fail(function (error) {
+    $("#errors").html("I'm sorry, there's been an error. Please try again.");
+  });
+}
+
+
+
 $(document).ready(function (){
-  handlebarsSetup()
+  handlebarsSetup();
 });
