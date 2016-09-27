f3f375aa49a570013a8ad83441ebff546611c6c4
jd2rogers2 <jd2rogers2@gmail.com>
2016-09-27 16:21:15 -0400

    instructor question 2

--- index.js
+++ index.js
@@ -4,32 +4,38 @@ function handlebarsSetup() {
 }
 
 $(document).ready(function (){
-  handlebarsSetup()
+  handlebarsSetup();
+  searchRepositories();
 });
 
 // provided code above
 // my code below
-function searchFor() {
-  $('a').on("click", searchRepositories(){
+function searchRepositories() {
+  $('a').on("click", function(){
+    debugger;
     var terms = $('#searchTerms').val();
-    // query github search api with above
+    $.get("https://api.github.com/search/repositories?q=" + terms, function(data) {
+      debugger;
+      // for (item in data) {
+      //   debugger;
+      // }
+      // display data in results div
+    }).fail(function(error){
+      displayError();
+    });
   });
 }
 
-$.get("https://api.github.com", function(data) {
-  // use data
-}).fail(function(error){
-  // use error
-});
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.");
+}
 
-// function searchFor() {
-//   $('a').on("click", searchRepositories(){
-//     var terms = $('#searchTerms').val();
-//     // query github search api with above
-//     $.get("https://api.github.com", function(data) {
-//       // use data
-//     }).fail(function(error){
-//       // use error
-//     });
-//   });
-// }
+function showCommits() {
+  $('#showCommits').on("click", function() {
+    $.get(" repo link ", function(data){
+      // display data in details div
+    }).fail(function(error){
+      displayError();
+    });
+  });
+}
