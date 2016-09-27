feffe1de1942fcbc0dc62ec0439541c2a5c97c99
jd2rogers2 <jd2rogers2@gmail.com>
2016-09-27 11:04:09 -0400

    ask a question

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+// provided code above
+// my code below
+function searchFor() {
+  $('a').on("click", searchRepositories(){
+    var terms = $('#searchTerms').val();
+    // query github search api with above
+  });
+}
+
+$.get("https://api.github.com", function(data) {
+  // use data
+}).fail(function(error){
+  // use error
+});
+
+// function searchFor() {
+//   $('a').on("click", searchRepositories(){
+//     var terms = $('#searchTerms').val();
+//     // query github search api with above
+//     $.get("https://api.github.com", function(data) {
+//       // use data
+//     }).fail(function(error){
+//       // use error
+//     });
+//   });
+// }
