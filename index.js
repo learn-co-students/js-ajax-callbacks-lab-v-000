fa246b091f82f82e415e51ab8f01f87672944648
gabe <gj+code@mail.co.de>
2016-09-26 23:39:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-function handlebarsSetup() {
-  //put any handlebars setup in here
-  Handlebars.registerPartial("userDetails", $("#user-details-partial").html());
+function displayError() {
+  $("#errors").html("I'm sorry, there's been an error. Please try again.");
 }
 
 function searchRepositories() {
+  clearErrors();
   var searchTerms = $('#searchTerms').val();
-  var searchURL = `https://api.github.com/search/repositories?q=${searchTerms}`
-  $.get(searchURL, function(data) {
-    var template = Handlebars.compile($('#results-handlebars-template').html());
-    $('#results').html(template(data));
-  }).fail(function (error) {
-    $("#errors").html("I'm sorry, there's been an error. Please try again.");
+  var searchURL = `https://api.github.com/search/repositories?q=${searchTerms}`;
+  $.get(searchURL, searchResults => {
+    $('#details').empty();
+    var template = Handlebars.compile($('#results-partial').html());
+    $('#results').html(template(searchResults));
+  }).fail(error => {
+    $('#results').empty();
+    $('#details').empty();
+    displayError();
   });
 }
 
-function 
+function showCommits(commit) {
+  var owner = commit.dataset.owner;
+  var repo = commit.dataset.repository;
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, commitDetails => {
+    var template = Handlebars.compile($('#commits-partial').html());
+    $('#details').html(template(commitDetails));
+  }).fail(error => {
+    displayError();
+  });
+}
+
+function clearErrors() {
+  $('#errors').empty();
+}
+
+function handlebarsSetup() {
+  //put any handlebars setup in here
+  Handlebars.registerPartial('userDetails', $('#user-details-partial').html());
+  Handlebars.registerPartial('resultsPartial', $('#results-partial').html());
+  Handlebars.registerPartial('commitsPartial', $('#commits-partial').html());
+}
 
 $(document).ready(function (){
   handlebarsSetup();
