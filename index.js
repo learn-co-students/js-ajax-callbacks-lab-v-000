4ae3565cca7042c8ac514bc583d7a8775a580108
jd2rogers2 <jd2rogers2@gmail.com>
2016-09-29 10:16:00 -0400

    instructor help

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
+$(document).ready(function (){
+  handlebarsSetup();
+});
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
 
-$(document).ready(function (){
-  handlebarsSetup();
-  searchRepositories();
-});
-
 // provided code above
 // my code below
+
 function searchRepositories() {
-  $('a').on("click", function(){
-    debugger;
-    var terms = $('#searchTerms').val();
-    $.get("https://api.github.com/search/repositories?q=" + terms, function(data) {
-      debugger;
-      // for (item in data) {
-      //   debugger;
-      // }
-      // display data in results div
-    }).fail(function(error){
-      displayError();
-    });
+  const url = "https://api.github.com/search/repositories?q=" + $('#searchTerms').val();
+  $.get(url, function(data) {
+    const template = Handlebars.compile($('#results-template').html());
+    $('#results').html(template(data));
+  }).fail(function(error){
+    displayError();
   });
 }
 
@@ -30,12 +24,14 @@ function displayError() {
   $('#errors').html("I'm sorry, there's been an error. Please try again.");
 }
 
-function showCommits() {
-  $('#showCommits').on("click", function() {
-    $.get(" repo link ", function(data){
-      // display data in details div
-    }).fail(function(error){
-      displayError();
-    });
+function showCommits(a_tag) {
+  const owner = a_tag.dataset.owner;
+  const repo = a_tag.dataset.repository;
+  const url = `https://api.github.com/repos/${owner}/${repo}/commits`;
+  $.get(url, function(data){
+    const template = Handlebars.compile($('#details-template').html());
+    $('#details').html(template(data));
+  }).fail(function(error){
+    displayError();
   });
 }
