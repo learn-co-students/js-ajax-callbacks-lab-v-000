448cd06da11c0636f538d6d51741a66956e1213a
toanhtran <toanh.t.tran@gmail.com>
2016-10-07 13:08:10 +0900

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    const template = Handlebars.compile($('#results-template').html())
+    $('#results').html(template(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+function showCommits(user) {
+  const owner = user.dataset.owner
+  const repo = user.dataset.repository
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, data => {
+    const template = Handlebars.compile($('#commits-template').html())
+    $('#details').html(template(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
 
 $(document).ready(function (){
-  handlebarsSetup()
+  handlebarsSetup();
 });
