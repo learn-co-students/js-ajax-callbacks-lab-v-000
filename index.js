aa7afaa1421866e48693a3bae0403b5d6c6cc696
rgylling <mikegylling@yahoo.com>
2016-10-10 10:12:26 -0700

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 function handlebarsSetup() {
-  //put any handlebars setup in here
-  Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
+  Handlebars.registerPartial("userDetails", $("#user-details-temp").html())
 }
 
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+function displayError() {
+  $("#errors").append("There has been an error, please try again.");
+};
+
+function searchRepositories() {
+  var searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data) {
+    var temp = Handlebars.compile($('#results-temp').html())
+    $('#results').html(temp(data))
+  }).fail(function() {
+    displayError()
+  })
+};
+
+function showCommits(commit) {
+  var owner = commit.dataset.owner
+  var repo = commit.dataset.repository
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, function(data) {
+    var temp = Handlebars.compile($('#commits-temp').html())
+    $('#details').html(temp(data))
+  }).fail(function(error) {
+    displayError()
+  });
+};
