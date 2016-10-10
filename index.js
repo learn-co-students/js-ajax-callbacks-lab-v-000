900ee4562f5d0041b75a07d1bb2442f7526e4198
kaileighrose <kaileighrose@gmail.com>
2016-10-10 14:03:08 -0400

    Done.

--- index.js
+++ index.js
@@ -9,52 +9,26 @@ function handlebarsSetup() {
 }
 
 function searchRepositories() {
-    $('#searchlink').on('click', function(event) {
-      event.preventDefault();
       var term = $('#searchTerms').val();
       var link = "https://api.github.com/search/repositories?q=" + term + "/";
     $.get(link, function(data) {
-      for (var i = 0; i < data["items"].length; i++) {
-      var url = data["items"][i].commits_url;
-      url = url.replace('{/sha}','');
-      $('#results')
-      .append("Name: " + data["items"][i].name + "<br>")
-      .append("URL: " + data["items"][i].html_url + "<br>")
-      .append("Description: " + data["items"][i].description + "<br>")
-      .append("Owner Login: " + data["items"][i].owner.login + "<br>")
-      .append("Owner Profile: " + data["items"][i].owner.url + "<br>")
-      .append("<img src='" + data["items"][i].owner.avatar_url + "'>" + "<br>")
-      .append("<a id='showCommits' href='" + url + "'>Show Commits</a>" + "<br><br>");
-      var template = Handlebars.compile($("#people-template").html());
-      var context = {login: data["items"][i].owner.login, avatar: data["items"][i].owner.avatar_url};
-      template(context);
-  };
-    showCommits();
-    return data;
+      const template = Handlebars.compile($('#results-template').html())
+      $('#results').html(template(data))
     }).fail(function(error) {
       displayError()
       });
-    });
   }
 
   
-function showCommits() {
-  $('#showCommits').on('click', function(event) {
-    event.preventDefault();
-    var a = document.getElementById("showCommits");
-    $.get(a.href, function(data) {
-      for (var i = 0; i < data.length; i++) {
-      $('#details')
-      .append("Name: " + data[i].commit.author.name + "<br>")
-      .append("SHA: " + data[i].sha + "<br>")
-      .append("Author Login: " + data[i].author.login + "<br>")
-      .append("<img src='" + data[i].author.avatar_url + "'>" + "<br>");
-    };
-    return data;
+function showCommits(el) {
+  const owner = el.dataset.owner
+  const repo = el.dataset.repository
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, data => {
+    const template = Handlebars.compile($('#commits-template').html())
+    $('#details').html(template(data))
     }).fail(function(error) {
       displayError()
       });
-  });
 }
 
 function displayError() {
