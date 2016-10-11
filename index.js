d138736f9d2a1973de085b16ee8b75e0512fff4a
krisnotchris <krishenderson@gmail.com>
2016-10-10 21:46:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-function handlebarsSetup() {
+  function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
 
+function searchRepositories() {
+  var searchTerms = $('#searchTerms').val();
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    var template = Handlebars.compile($("#results-template").html());
+    $("#results").html(template(data));
+  }).fail(displayError());
+}
+
+function showCommits(element) {
+  var owner = element.dataset.owner
+  var repo = element.dataset.repository
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, data => {
+    var template = Handlebars.compile($('#show-commits-template').html());
+    $('#details').html(template(data));
+  })
+}
+
+function displayError() {
+  $("#errors").html("I'm sorry, there's been an error. Please try again.");
+}
+
+
 $(document).ready(function (){
-  handlebarsSetup()
+  handlebarsSetup();
+
 });
