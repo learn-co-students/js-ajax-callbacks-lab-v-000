4d833d80776bad14157feab5f7e30edde7e616d7
auranbuckles <auran59@gmail.com>
2016-10-16 19:16:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
+$(document).ready(function (){
+  handlebarsSetup();
+
+  $.ajax({
+  	type: 'GET',
+  	url: 'https://api.github.com/search/repositories?q=fire'
+  });
+
+  $('#search').on('click', searchRepositories);
+});
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
 
-$(document).ready(function (){
-  handlebarsSetup()
-});
+function searchRepositories(event) {
+  var search = $('#searchTerms').val()
+  var url = 'https://api.github.com/search/repositories?q=' + search
+  $.get(url, data => {$('#results').html(Handlebars.compile($('#results-template').html())(data))})
+    .fail(function(error)
+      {displayError()});
+}
+
+function showCommits(repoLink){
+  var user = repoLink.dataset.owner
+  var repo = repoLink.dataset.repository
+  $.get(`https://api.github.com/repos/${user}/${repo}/commits`, data => {
+    const template = Handlebars.compile($('#commits-template').html())
+    $('#details').html(template(data))
+  }).fail(error =>{
+    displayError()
+  })
+}
+
+function displayError(){
+  $('#errors').html("There's been an error. Please try again.");
+}
