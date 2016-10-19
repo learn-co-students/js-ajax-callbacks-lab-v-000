9439b2a1dd1fe10a2b6038273be3e2276025d576
jbok4 <jbok4@aol.com>
2016-10-19 16:19:08 -0400

    what the f

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
 
+function searchRepositories() {
+  var searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+      const template = Handlebars.compile($('#results-template').html())
+      $('#results').html(template(data))
+    }).fail(error => {
+      displayError()
+    })
+}
+
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+
+
+
+
