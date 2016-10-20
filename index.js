da637c7ea6d434a178ef74e6fac2fac1fc6cdc38
KerryAlsace <kerrydougherty@gmail.com>
2016-10-20 13:53:32 -0400

    Done.

--- index.js
+++ index.js
@@ -5,4 +5,19 @@ function handlebarsSetup() {
 
 $(document).ready(function (){
   handlebarsSetup()
+
+  function searchRepositories(query) {
+    var url = 'https://api.github.com/search/repositories?q=' + query
+    $.get(url, function(response) {
+      $('#results').html(response);
+      // return console.log(response);
+    }).fail(function(error) {
+      console.log('Something went wrong: ' + error);
+    });
+  }
+
+  $('#submit').on('click', function(){
+    var searchTerm = $('#searchTerms').val();
+    return searchRepositories(searchTerm);
+  });
 });
