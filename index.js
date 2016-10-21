cab81657eff1373317426bc7adc479aff12054ff
Christian Whitesides <whitesidescr@gmail.com>
2016-10-21 17:59:54 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Use as reference: https://www.sitepoint.com/a-beginners-guide-to-handlebars/
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
