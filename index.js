e148cebc75366aa8216e879c9d10657680cdf5ad
Kathleen <kayjsmyth@gmail.com>
2016-10-23 20:25:04 -0400

    Done.

--- index.js
+++ index.js
@@ -7,25 +7,48 @@ function handlebarsSetup() {
 }
 
 $(document).ready(function (){
-	searchRepositories();
-  	handlebarsSetup()
+  	handlebarsSetup();
+  	handleSearchLink();
+  	handleCommitsLink();
 });
 
-function searchRepositories(){
+function handleSearchLink() {
 	$('a#searchLink').on('click', function(event) {
 		event.preventDefault();
-		var searchTerms = $('input#searchTerms').val();
-		var query = "https://api.github.com/search/repositories?q=" + searchTerms;
-		$.get(query, function(results) {
-			$("#results").html(template());
-		}).fail(displayError(error));
-		return false;
+		searchRepositories();
 	});
 }
 
-function displayError(error) {
+function handleCommitsLink() {
+	$('a#commitsLink').on('click', function(event) {
+		event.preventDefault();
+		var commitsQueryStuff = $(this).innerHtml();
+		console.log(commitsQueryStuff);
+		showCommits(commitsQueryStuff);
+	});
+}
+
+function searchRepositories(){
+	var searchTerms = $('input#searchTerms').val();
+	var query = "https://api.github.com/search/repositories?q=" + searchTerms;
+	var source   = $("#results-template").html();
+	var template = Handlebars.compile(source);
+
+	$.get(query, function(results) {
+		$("#results").html(template(results));
+		console.log(results.items);
+	}).fail(displayError());
+}
+
+function displayError() {
 	var errorText = "I'm sorry, there's been an error. Please try again.";
 	$('div#errors').append(errorText);
 }
 
+function showCommits(commitsQueryStuff) {
+	var commitsQuery = "http://api.github.com/repos/" + commitsQueryStuff + "/commits";
+	$.get(commitsQuery, function(results) {
+		$("#results").html(template(results));
+	}).fail(displayError());
+}
 
