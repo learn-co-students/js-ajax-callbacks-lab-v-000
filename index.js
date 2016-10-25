a96f8952729964d01477e840aa7a608ba9c4e922
SiamKing <thesiameseelectric@gmail.com>
2016-10-25 11:21:50 -0700

    worst lab evah

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 function handlebarsSetup() {
   //put any handlebars setup in here
-  Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
+  Handlebars.registerPartial("userDetails", $("#user-details-partial").html());
+}
+
+function searchRepositories() {
+  var searchTerms = $("#searchTerms").val();
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data) {
+    var template = Handlebars.compile($("#results-template").html());
+    $("#results").html(template(data));
+  }).fail(function(error) {
+    displayError(error);
+  });
+}
+
+function showCommits(el) {
+  var owner = el.dataset.owner;
+  var repo = el.dataset.repository;
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, function(data) {
+    var template = Handlebars.compile($("#commits-template").html());
+    $("#details").html(template(data));
+  }).fail(function(error) {
+    displayError(error);
+  });
+}
+
+function displayError(){
+  $('#errors').html("I'm sorry, there's been an error. Please try again.");
 }
 
 $(document).ready(function (){
-  handlebarsSetup()
+  handlebarsSetup();
 });
