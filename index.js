45402ac463b6ce4696f10faa3ac3e8e17210c30a
jbok4 <jbok4@aol.com>
2016-10-28 12:17:36 -0400

    complete

--- index.js
+++ index.js
@@ -2,26 +2,57 @@ function displayError() {
   $('#errors').html("I'm sorry, there's been an error. Please try again.")
 }
 
+
+function searchRepositories() {
+    var repos = $('#searchTerms').val();
+    var url = "https://api.github.com/search/repositories?q="+repos;
+
+    $.get(url)
+      .done(function(data){
+        var template = $('#results-template').html();
+        var context = data;
+        var templateScript = Handlebars.compile(template);
+        //Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
+        var html = templateScript(context);
+        
+        $('div[id=results] tbody').html(html);
+        $('#errors').html('')
+    })
+    .fail(function() {
+      displayError();
+
+    });
+
+
+
+}
+
+function showCommits(el) {
+    var repos = el.dataset.repository;
+    var owner = el.dataset.owner;
+    var url = "https://api.github.com/repos/" + owner + "/" + repos + "/commits\?";
+
+    $.get(url)
+      .done(function(data){
+        var template = $('#commits-template').html();
+        var context = data;
+        var templateScript = Handlebars.compile(template);
+        var html = templateScript(context);
+
+        $('div[id=details] tbody').html(html);
+    })
+    .fail(function () {
+      displayError();
+    });
+
+}
+
+
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
-
-function searchRepositories() {
-  var searchTerms = $('#searchTerms').val()
-  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
-      const template = Handlebars.compile($('#results-template').html())
-      $('#results').html(template(data))
-    }).fail(error => {
-      displayError()
-    })
-}
-
 $(document).ready(function (){
-  handlebarsSetup()
+  handlebarsSetup();
 });
-
-
-
-
-
