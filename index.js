ff81d4462958050744836e3a2a41cc2802ac7198
Traci7822 <Tracir@gmail.com>
2016-11-08 15:07:59 -0700

    Done.

--- index.js
+++ index.js
@@ -8,17 +8,26 @@ $(document).ready(function (){
 });
 
 function searchRepositories(){
-  var searchTerms = $('#searchTerms').val();
-  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data =>
-    {const template = Handlebars.compile($('#results-template').html())
+  debugger;
+  const searchTerms = $('#searchTerms').val();
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    const template = Handlebars.compile($('#results-template').html())
     $('#results').html(template(data))
+    debugger;
   }).fail(error => {
     displayError()
   })
 }
 
-function showCommits(){
-  
+function showCommits(user){
+  const owner = user.dataset.owner
+  const repo = user.dataset.repository
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, data => {
+    const template = Handlebars.compile($('#commits-template').html())
+    $('#details').html(template(data))
+  }).fail(error => {
+    displayError()
+  })
 }
 
 function displayError() {
