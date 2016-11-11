1943c257a0af5966d6d15a542745eae82a8a5fb2
Caleb Faught <faughtcj@gmail.com>
2016-11-11 12:07:53 -0600

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ function handlebarsSetup() {
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
 
+function displayError(error) {
+  $('#errors').html("I'm sorry, there was an error: ")
+}
+
+function searchRepositories() {
+  var searchTerm = $('#searchTerms').val();
+  $.get('https://api.github.com/search/repositories?q=' + searchTerm, function(data) {
+    var template = Handlebars.compile($('#results-template').html());
+    $('#results').html(template(data));
+  }).fail(function(error) {
+    displayError(error);
+  });
+}
+
+function showCommits(params) {
+  var owner = params.dataset.owner;
+  var repo = params.dataset.repository;
+
+  $.get('https://api.github.com/repos/' + owner + '/' + repo + '/commits', function(data) {
+    var template = Handlebars.compile($('#commits-template').html());
+    $('#details').html(template(data));
+  }).fail(function(error) {
+    displayError(error);
+  });
+}
+
 $(document).ready(function (){
   handlebarsSetup()
 });
