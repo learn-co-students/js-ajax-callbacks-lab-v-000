a071f2563d60b4a313e17d6a71f479df704a03fa
lenn hypolite <lennhydesign@gmail.com>
2016-11-12 11:40:42 -0500

    initial com

--- index.js
+++ index.js
@@ -4,5 +4,35 @@ function handlebarsSetup() {
 }
 
 $(document).ready(function (){
-  handlebarsSetup()
+  // handlebarsSetup()
+  searchRepositories();
 });
+// const searchTerms = $('#searchTerms').val()
+// console.log(searchTerms);
+//   $.get("https://api.github.com/search/repositories"+"?q="+searchTerms, function(response) {
+//     $("#results").html(response);
+//   }).fail(function(error) {
+//     // This is called when an error occurs
+//       $('#errors').text("Im sorry there's been an error. Please try again.");
+// });
+
+function searchRepositories() {
+  var searchTerms = $('#searchTerms').val();
+   $.ajax({
+     url: "https://api.github.com/search/repositories"+"?q="+searchTerms,
+     type: "GET",
+     dataType: "text",
+     success: successFn,
+     error: errorFn,
+     complete: function(xhr, status){
+      console.log("The request is completed");
+     }
+    //  $('#results').html('')
+  });
+}
+function successFn(data) {
+  console.log('success',data);
+}
+function errorFn(xhr, status, strErr) {
+  $("#results").html(status + xhr +strErr + "I'm sorry, there's been an error. Please try again.");
+}
