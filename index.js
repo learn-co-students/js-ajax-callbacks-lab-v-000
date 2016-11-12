0a8a918262e4b81fba546bd8e85cf3602ac6da00
Ann Lee <aplee29@users.noreply.github.com>
2016-11-12 03:16:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+$(document).ready(function (){
+  handlebarsSetup();
+});
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
 
-$(document).ready(function (){
-  handlebarsSetup()
-});
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val();
+  const url = 'https://api.github.com/search/repositories?q=' + searchTerms;
+
+  $.get(url, function(data) {
+    const template = Handlebars.compile($('#results-template').html());
+    $('#results').html(template(data));
+  }).fail(function(error) {
+    displayError();
+  });
+}
+
+function showCommits(element) {
+  const user = element.dataset.owner;
+  const repo = element.dataset.repository;
+  const url = "https://api.github.com/repos/" + `${user}/${repo}/commits`;
+
+  $.get(url, function(data) {
+    const template = Handlebars.compile($('#commits-template').html());
+    $('#details').html(template(data));
+  });
+}
+
+function displayError(){
+  $('#errors').html("I'm sorry, there's been an error. Please try again.");
+}
