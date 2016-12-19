d6819436da164658cee7a973d4d6e9dde001f071
ldgarber <leah.garber88@gmail.com>
2016-12-19 16:31:22 -0500

    working

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+"use strict"; 
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
+}
+
+function displayError(error) {
+  var message = "I'm sorry, there's been an error. Please try again."; 
+  $("#errors").html(message); 
 }
 
 $(document).ready(function (){
