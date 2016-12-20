c495a075e6adc552ce1995a57816f5d2a60b2e25
ldgarber <leah.garber88@gmail.com>
2016-12-20 16:51:44 -0500

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,35 @@
 
 function handlebarsSetup() {
   //put any handlebars setup in here
-  Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
+  Handlebars.registerPartial("userDetails", $("#user-details-partial").html()); 
+  Handlebars.registerPartial("resultsTemplate", $("#results-template").html()); 
+  Handlebars.registerPartial("commitsTemplate", $("#commits-template").html()); 
+}
+
+function handleSearchClick(){
+  $("#search").on("click", searchRepositories);  
+}
+
+function searchRepositories(){
+  var searchTerm = $("#searchTerms").val(); 
+  var url = "https://api.github.com/search/repositories?q=" + searchTerm; 
+  $.get(url, function(data){
+    var template = Handlebars.compile($('#results-template').html()); 
+    $('#results').html(template(data)); 
+  }).fail(function(error) {
+    displayError(error);  
+  }); 
+}
+
+function showCommits(e){
+  var owner = e.dataset.owner; 
+  var repo = e.dataset.repository; 
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, function(data) {
+    var template = Handlebars.compile($('#commits-template').html()); 
+    $('#details').html(template(data)); 
+  }).fail(function(error) {
+    displayError(error);  
+  })
 }
 
 function displayError(error) {
@@ -11,5 +39,6 @@ function displayError(error) {
 }
 
 $(document).ready(function (){
-  handlebarsSetup()
+  handleSearchClick(); 
+  handlebarsSetup(); 
 });
