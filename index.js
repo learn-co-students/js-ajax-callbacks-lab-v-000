ded64ff1b3ad7121267736e52254ccc95648b324
Brianna Titone <briannactitone@gmail.com>
2016-12-24 15:43:31 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
+function searchRepositories(searchTerms){
+  $.get("https://api.github.com/repos/jquery/jquery/commits?author="+searchTerm, function(response) {
+    // Here we are getting the element on the page with the id of sentences and
+    // inserting the response
+    $("#results").html(response);
+  });
+}
+
+function showCommits(){
+  //display details in details div
+
+}
 
 $(document).ready(function (){
   handlebarsSetup()
