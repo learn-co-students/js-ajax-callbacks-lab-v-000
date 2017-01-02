5c6208ed9a8b4c331b70972f30cb0dc9f8a1739f
Brianna Titone <briannactitone@gmail.com>
2017-01-02 03:39:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+var responses;
+
 function handlebarsSetup() {
   //put any handlebars setup in here
-  Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
+  Handlebars.registerPartial("userDetails", $("#user-details-partial").html());
 }
-function searchRepositories(searchTerms){
-  $.get("https://api.github.com/repos/jquery/jquery/commits?"+searchTerms, function(response) {
-    // Here we are getting the element on the page with the id of sentences and
-    // inserting the response
-    $("#results").html(response);
+
+function searchRepositories(){
+  searchTerms = $('#searchTerms').val();
+  $.get("https://api.github.com/repos/jquery/jquery/commits?"+searchTerms).done(function(response) {
+    	var template = $('#handlebars-template').html();
+      var resp = Handlebars.compile(template);
+      for(var i=0;i<response.length;i++){
+      	  var newValue = resp((response[i]));
+          $('#results').append(newValue);
+      }
+
+  }).fail(function() {
+     $("#errors").html( "I'm sorry, there's been an error. Please try again." );
   });
 }
 
@@ -16,5 +26,5 @@ function showCommits(){
 }
 
 $(document).ready(function (){
-  handlebarsSetup()
+  handlebarsSetup();
 });
