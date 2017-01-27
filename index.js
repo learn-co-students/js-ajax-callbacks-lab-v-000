4cc74811d92bf5a4f292502c4fc725e371fcfec1
bmolina-nyc <bruce.molina.81@gmail.com>
2017-01-26 20:17:22 -0500

    Done.

--- index.js
+++ index.js
@@ -4,15 +4,49 @@ function handlebarsSetup() {
 }
 
 $(document).ready(function (){
-  handlebarsSetup()
+  handlebarsSetup();
+});
+
+var searchTerms = $('#searchTerms').val()
+var $results = $('#results');
+
+$(function searchRepositories(){
+  $.ajax({
+    type: 'GET',
+    url: "https://api.github.com/search/repositories?q=${searchTerms}",
+    success: function(repos){
+      $.each(repos, function(i, repodata){
+        $results.append('<li> ' + repodata +  '</li>')
+      });
+    }
+  });
 });
 
 
-function searchRepositories(){
-  url: "https://api.github.com/search/repositories?q=${searchTerms}"
-}
+// $(function searchRepositories(){
+//     $.get("https://api.github.com/search/repositories?q=${searchTerms}"), function(repos){
+//       $.each(repos, function(i, repodata){
+//         $results.append('<li> ' + repodata +  '</li>')
+//       });
+//     }
+// });
 
 
-function displayError(){
 
-}
+// function searchRepositories(){
+//   url: "https://api.github.com/search/repositories?q=${searchTerms}"
+// }
+
+
+// function displayError(){
+
+// }
+
+
+// $.get("this_doesnt_exist.html", function(data) {
+//   // This will not be called because the .html file request doesn't exist
+//   doSomethingGood();
+// }).fail(function(error) {
+//   // This is called when an error occurs
+//   console.log('Something went wrong: ' + error);
+// });
