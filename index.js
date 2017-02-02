2acf0163c0384342db8e3f91e5710bbc7b064c75
lcorr8 <l.corr.v@gmail.com>
2017-02-02 22:49:58 +0100

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+
+// where to start # 1
+//search repositories link = 'https://api.github.com/search/repositories?q='
+
+function searchRepositories(){
+  //array of terms to place in url
+  var searchTerms = $('#searchTerms').val().split(' ').join('+')
+  var baseUrl = "https://api.github.com/search/repositories?q="  
+      //debugger;
+  $.get((baseUrl + searchTerms), function (data){
+    //do something with the data?
+    console.log(data)
+  }).fail(function(error){
+    displayError();
+    // action when error happens
+  })
+};
+
+function displayError(){
+// add errors to error div
+  $('#errors').text("I'm sorry, there's been an error. Please try again." )
+};
+
+
