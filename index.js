960f3b94ca64300864ddf9a9560609cb006bad03
Joe <jsbenning4@gmail.com>
2017-02-02 13:32:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 function handlebarsSetup() {
   //put any handlebars setup in here
-  Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
+  //Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
 
 $(document).ready(function (){
-  handlebarsSetup()
+  //handlebarsSetup()
+
+  $("form").on("submit", function(event) { 
+    var searchTerms = $("#searchTerms").val();
+    var gitRepos = "https://api.github.com/search/repositories?q=" + searchTerms;
+      $.get(gitRepos, function(data) {
+        console.log("Hurrah!");  
+      }).fail(function(error) {
+        console.log("Poop!");
+      })
+    event.preventDefault();
+  })      
 });
+
+
+
+
+/*
+
+  function requestJSON(url, callback) {
+    $.ajax({
+      url: url,
+      complete: function(xhr) {
+        callback.call(null, xhr.responseJSON);
+      }
+    });
+  }
+
+$(function(){
+  $('#ghsubmitbtn').on('click', function(e){
+    e.preventDefault();
+    $('#ghapidata').html('<div id="loader"><img src="css/loader.gif" alt="loading..."></div>');
+    
+    var username = $('#ghusername').val();
+    var requri   = 'https://api.github.com/users/'+username;
+    var repouri  = 'https://api.github.com/users/'+username+'/repos';
+    
+    requestJSON(requri, function(json) {
+  */
