467c1adc2d6d1ea3247fa1e829b1498ae4e4a51f
Joe <jsbenning4@gmail.com>
2017-02-03 21:41:39 -0500

    Done.

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ $(document).ready(function (){
     var searchTerms = $("#searchTerms").val();
     var gitRepos = "https://api.github.com/search/repositories?q=" + searchTerms;
       $.get(gitRepos, function(data) {
-        console.log("Hurrah!");  
+        var repos = data.items
+        $.map(repos, function(repo, index) {
+          var name = repo.name;
+          var description = repo.description;
+          var link = repo.svn_url;
+          $("#results").append("<p>Name: " + name.name + "</p><p>Description: " + name.description + `</p><p>Link: <a href="${name.svn_url}"></a><br>` );
+        })  
       }).fail(function(error) {
-        console.log("Poop!");
+        console.log("Something didn't work: " + error);
       })
     event.preventDefault();
   })      
@@ -22,6 +28,16 @@ $(document).ready(function (){
 
 
 /*
+
+  for (var i = 1; i < 100; i++) {
+  .   console.log( "Hello World the " + i + " time" );
+  . }
+
+for(var key in myObject) {
+    if(myObject.hasOwnProperty(key)) {
+        myObject[key] *= 2;
+    }
+}
 
   function requestJSON(url, callback) {
     $.ajax({
