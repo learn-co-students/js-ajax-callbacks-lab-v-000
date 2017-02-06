8a488aea665e2ee1838cec1ddc7a9a8fee8c577b
Joe <jsbenning4@gmail.com>
2017-02-06 11:33:35 -0500

    Done.

--- index.js
+++ index.js
@@ -17,21 +17,26 @@ $(document).ready(function (){
   }
 
   getCommits = function(link) {
+    $("#details").empty();
+    $("#errors").empty();
     $.get(link)
     .done(function(data) {
       for (var i = 0; i < data.length; i++) {
-        var num = i;
-        var sha = data[i].sha;    
-        var author = data[i]["author"]["login"];
-        var avatar = data[i]["author"]["avatar_url"];
-
-        $("#details").append(`<div class="commits"><p><b>Commit number ${i}</b></p><p>SHA: ${sha}</p><p></p><p>Author: ${author}</p><p><img src=${avatar} style="width: 50%; height: 50%"/></p></div>`); 
-      }
+        var num = i + 1
+        var sha = data[i].sha || "None provided"; 
+        var author = data[i]["commit"]["author"]["name"] || "None provided";   
+        var authorLogin = data[i]["author"]["login"] || "None provided";    
+        var avatar = data[i]["author"]["avatar_url"] || "None provided";
+        $("#details").append(`<div class="commits"><p><b>Commit number ${num}</b></p><p>SHA: ${sha}</p><p></p><p>Author: ${author}</p><p>Author Login: ${authorLogin}<p><img src=${avatar} style="width: 50%; height: 50%"/></p><hr></div>`); 
+        }
+      }).fail(function(error) {
+        displayError();
     })
   }
 
 
   $("form").on("submit", function(event) { 
+    clearPage(); 
     var searchTerms = $("#searchTerms").val();
     var gitRepos = "https://api.github.com/search/repositories?q=" + searchTerms;
       $.get(gitRepos)
@@ -41,11 +46,11 @@ $(document).ready(function (){
             var name = repo.name;
             var description = repo.description || "None provided";
             var link = repo.html_url;
-            var userLogin = repo["owner"]["login"];
-            var userAvatar = repo["owner"]["avatar_url"];
-            var userProfileLink = repo["owner"]["html_url"];
+            var userLogin = repo["owner"]["login"] || "None provided";
+            var userAvatar = repo["owner"]["avatar_url"] || "None provided";
+            var userProfileLink = repo["owner"]["html_url"] || "None provided";
             var commitsLink = repo.commits_url.slice(0, -6);
-            clearPage();  
+             
             $("#results").append(`<p><b>Repo Name: ${name}</b></p><p>Description: ${description}</p><p><a href=${link} target="_blank">Repo Details Here</a></p><p>Creator: ${userLogin}</p><p><img src=${userAvatar}/></p><p><a href=${userProfileLink} target="_blank">Creator Profile</a></p><div class="commits"><button type="button"  value="View Commits" id=${commitsLink} onclick="getCommits(this.id);">View Commits</button></div></p><hr><br><br>` );
           })  
         }).fail(function(error) {
@@ -53,48 +58,5 @@ $(document).ready(function (){
       })
     event.preventDefault();
   })      
-
-
-
-  // $(".commits").on("click", function() {
-  //   alert("button clicked")//, id " + this.class + ", text" + this.innerHTML); 
-
-
-
-
-
 });
 
-
-/*
-
-  for (var i = 1; i < 100; i++) {
-  .   console.log( "Hello World the " + i + " time" );
-  . }
-
-for(var key in myObject) {
-    if(myObject.hasOwnProperty(key)) {
-        myObject[key] *= 2;
-    }
-}
-
-  function requestJSON(url, callback) {
-    $.ajax({
-      url: url,
-      complete: function(xhr) {
-        callback.call(null, xhr.responseJSON);
-      }
-    });
-  }
-
-$(function(){
-  $('#ghsubmitbtn').on('click', function(e){
-    e.preventDefault();
-    $('#ghapidata').html('<div id="loader"><img src="css/loader.gif" alt="loading..."></div>');
-    
-    var username = $('#ghusername').val();
-    var requri   = 'https://api.github.com/users/'+username;
-    var repouri  = 'https://api.github.com/users/'+username+'/repos';
-    
-    requestJSON(requri, function(json) {
-  */
