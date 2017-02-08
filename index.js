567dc0341c31871f17999a096fc2071059abf3aa
Joe <jsbenning4@gmail.com>
2017-02-08 13:32:47 -0500

    Done.

--- index.js
+++ index.js
@@ -16,18 +16,18 @@ $(document).ready(function (){
     $("#errors").append("I'm sorry, there's been an error. Please try again.");
   }
 
-  showCommits = function(link) {
+  getCommits = function(link) {
     $("#details").empty();
     $("#errors").empty();
     $.get(link)
     .done(function(data) {
       for (var i = 0; i < data.length; i++) {
+        console.log(data[i])
         var num = i + 1
-        var sha = data[i].sha || "None provided"; 
-        var author = data[i]["commit"]["author"]["name"] || "None provided";   
-        var authorLogin = data[i]["author"]["login"] || "None provided";    
+        var sha = data[i].sha || "None provided";    
+        var author = data[i]["author"]["login"] || "None provided";
         var avatar = data[i]["author"]["avatar_url"] || "None provided";
-        $("#details").append(`<div class="commits"><p><b>Commit number ${num}</b></p><p>SHA: ${sha}</p><p></p><p>Author: ${author}</p><p>Author Login: ${authorLogin}<p><img src=${avatar} style="width: 50%; height: 50%"/></p><hr></div>`); 
+        $("#details").append(`<div class="commits"><p><b>Commit number ${num}</b></p><p>SHA: ${sha}</p><p></p><p>Author: ${author}</p><p><img src=${avatar} style="width: 50%; height: 50%"/></p></div>`); 
         }
       }).fail(function(error) {
         displayError();
@@ -50,13 +50,25 @@ $(document).ready(function (){
             var userAvatar = repo["owner"]["avatar_url"] || "None provided";
             var userProfileLink = repo["owner"]["html_url"] || "None provided";
             var commitsLink = repo.commits_url.slice(0, -6);
+            
+          
              
-            $("#results").append(`<p><b>Repo Name: ${name}</b></p><p>Description: ${description}</p><p><a href=${link} target="_blank">Repo Details Here</a></p><p>Creator: ${userLogin}</p><p><img src=${userAvatar}/></p><p><a href=${userProfileLink} target="_blank">Creator Profile</a></p><div class="commits"><button type="button"  value="View Commits" id=${commitsLink} onclick="showCommits(this.id);">View Commits</button></div></p><hr><br><br>` );
+            $("#results").append(`<p><b>Repo Name: ${name}</b></p><p>Description: ${description}</p><p><a href=${link} target="_blank">Repo Details Here</a></p><p>Creator: ${userLogin}</p><p><img src=${userAvatar}/></p><p><a href=${userProfileLink} target="_blank">Creator Profile</a></p><div class="commits"><button type="button"  value="View Commits" id=${commitsLink} onclick="getCommits(this.id);">View Commits</button></div></p><hr><br><br>` );
           })  
         }).fail(function(error) {
         displayError();
       })
     event.preventDefault();
-  })      
-});
+  })   
+})     
+
+
+
+
+
+
+
+
+
+
 
