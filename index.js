2d58b956cc8356c5071ccccb1905bbf8d499f92b
lcorr8 <l.corr.v@gmail.com>
2017-02-08 16:19:50 +0100

    Done.

--- index.js
+++ index.js
@@ -12,30 +12,30 @@ $(document).ready(function (){
 //search repositories link = 'https://api.github.com/search/repositories?q='
 
 function searchRepositories(){
-  //array of terms to place in url
   var searchTerms = $('#searchTerms').val().split(' ').join('+')
   var baseUrl = "https://api.github.com/search/repositories?q="  
-      //ajax get request. first parameter is the URL with the search terms
-      // second parameter is a function handles the returned data from the API
-  //$.get((baseUrl + searchTerms), function (data){
-    //console.log(data)
-  // Instructions: Display the collection of repositories inside the results div.
+  
+  //ajax get request. first parameter is the URL with the search terms
+  // second parameter is a function handles the returned data from the API
+    // Instructions: Display the collection of repositories inside the results div.
 
+  //$.get((baseUrl + searchTerms), function (data){
+    
     //create a template variable to hold the compiled template. 
     //pass id of the element holding the template
-   // var template = Handlebars.compile( $('#results-template').html() )
+    //var template = Handlebars.compile( $('#results-template').html() )
 
-    //pass template and data to the div in the index where you to display your results
-   // $('#results').html(template(data))
+    //pass template and data to the div in the index where you are to display your results
+    //$('#results').html(template(data))
 
-  // when there is an error with the ajax request this gets called
- // }).fail(function(error){
-  //  displayError();
+  // when there is an error with the ajax request call the error displaying function
+  //}).fail(function(error){
+    //displayError();
   //})
 
   $.get(baseUrl + searchTerms)
     .done( data => {
-      var template = Handlebars.compile( $('#results-template').html());
+      var template = Handlebars.compile( $('#results-template').html())
       $('#results').html(template(data))
     })
     .fail(err => displayError() )
@@ -46,26 +46,20 @@ function displayError(){
 };
 
 function showCommits(item){
-
-  //return console.log(item)
-
   //get commits from github api
   const owner = item.dataset.owner
   const repo = item.dataset.repository
-
   var commitsUrl = `https://api.github.com/repos/${owner}/${repo}/commits`
-  $.get(commitsUrl, function(data){
+  
+  $.get(commitsUrl, data => {
+      //compile a template 
       var template = Handlebars.compile( $("#show-commits-template").html())
+      //pass template and data to the id of the div that is to display it
       $("#details").html(template(data))
-
-  //  debugger;
-    //compile a template and pass the data in order to display it?
-
+  //handle errors    
   }).fail(error => {
     displayError()
   })
  
 };
 
-//Handlebars.registerPartial('userDetails', '{{owner}} </{{tagName}}>')
-//searchRepositories()
