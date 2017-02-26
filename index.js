3a106afb0ccd56bdf7faa44f859ce1aacf0b19b8
Adam Lucia <lucia.adam@gmail.com>
2017-02-26 04:46:01 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+function searchRepositories() {
+  var terms = $('#searchTerms')
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepoResults());
+  req.open("GET", `https://api.github.com/search/repositories?q=${terms}`)
+}
+
+function showRepoResults(event, data) {
+  const repos = JSON.parse(this.responseText);
+  const src = document.getElementById('user-details-partial').innerHTML;
+  const template = Handlebars.compile(src);
+  const resultRepoList = template(repos);
+  document.getElementById("results").innerHTML = resultRepoList;
+}
