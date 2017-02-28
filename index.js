0898c28c65a3d444758a3f030d02a234bd222bfa
johnram528 <johnram528@gmail.com>
2017-02-28 07:32:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+$("#search").on('click', searchRepositories);
+
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
@@ -6,3 +10,28 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+
+
+
+function showCommits(el) {
+  var owner = el.dataset.owner;
+  var repo = el.dataset.repository;
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, function(results) {
+    var source = $("#commits-template").html();
+    var template = Handlebars.compile(source);
+    $('#details').html(template(results));
+  }).fail(displayError());
+}
+
+function searchRepositories() {
+  var input = $('#searchTerms').val();
+  var src = $("#results-template").html()
+  var template = Handlebars.compile(src)
+  $.get (`https://api.github.com/search/repositories?q=${input}`, results =>
+     $('#results').html(template(results))).fail(displayError())
+}
+  
+function displayError(){
+  $('#errors').html("I'm sorry, there's been an error. Please try again.");
+}
