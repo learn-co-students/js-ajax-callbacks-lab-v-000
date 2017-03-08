99e4f32caac11251d3fbf28e6976757bf6dd278a
Brad Watson <brad.watson.orlando@gmail.com>
2017-03-08 11:04:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function searchRepositories() {
+  var searchTerms = $("#searchTerms").val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    var src = document.getElementById('results-template').innerHTML
+    var template = Handlebars.compile(src)
+    $("#results").html(template(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+function showCommits(el) {
+  var owner = el.dataset.owner
+  var repo = el.dataset.repository
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, data => {
+    var src = document.getElementById('commits-template').innerHTML
+    var template = Handlebars.compile(src)
+    $("#details").html(template(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
