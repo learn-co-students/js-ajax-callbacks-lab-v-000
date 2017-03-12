1766bb1d4da9d79d6a5add811e0fdac800932bbb
Adam Friedl <ajfriedl@gmail.com>
2017-03-12 17:10:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+function displayError() {
+	$('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function searchRepositories() {
+	const url = 'https://api.github.com/search/repositories?q='
+	const searchTerms = $('#searchTerms').val()
+	$.get(url + searchTerms, data => {
+		const template = Handlebars.compile($('#results-template').html())
+		$('#results').html(template(data))
+	}).fail(error => displayError())
+}
+
+function showCommits(el) {
+	const owner = el.dataset.owner
+	const repo = el.dataset.repository
+	const url = 'https://api.github.com/repos/' + owner + '/' + repo + '/commits'
+	$.get(url, data => {
+		const template = Handlebars.compile($('#commits-template').html())
+		$('#details').html(template(data))
+	}).fail(error => displayError())
+}
+
+
+
+
+
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
@@ -6,3 +34,4 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
