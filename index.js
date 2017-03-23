719f5f3fb71e98eccd497acefc63fb6fc2655d2b
Nathan Robbert <admin@bigrocketgames.com>
2017-03-22 21:37:15 -0700

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val()
+  $.get("https://api.github.com/search/repositories?q=" + searchTerms + "/", data => {
+    console.log(data);
+    const template = Handlebars.compile($('#results-template').html())
+    $('#results').html(template(data))
+  }).fail(error => {
+    displayError();
+  });
+}
+
+function showCommits(el) {
+  const repo = el.dataset.repository
+  const owner = el.dataset.owner
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, data => {
+    console.log(data);
+    const template = Handlebars.compile($('#details-template').html())
+    $('#details').html(template(data))
+  }).fail(error => {
+    displayError();
+  });
+}
