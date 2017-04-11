edf323d2eb1832c015eaee7434e2cbd781ab4d08
mcclains2003 <mcclains2003@gmail.com>
2017-04-11 15:50:40 -0500

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+function searchRepositories() {
+  var criteria = $("#searchTerms").val();
+
+  $.get('https://api.github.com/search/repositories?q=' + criteria, data => {
+    template = Handlebars.compile($('#results-template').html())
+    $('#results').html(template(data.items))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+function showCommits(element) {
+  var owner = element.dataset.owner;
+  var repo = element.dataset.repository;
+
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, data => {
+    const template = Handlebars.compile($('#commits-template').html())
+    $('#details').html(template(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
