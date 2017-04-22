7b09addbd04783337b0726abedfc15f385b3b9e1
Ivan Pilot <ivanpilot@gmail.com>
2017-04-22 19:34:33 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,32 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+function searchRepositories(){
+  var search = document.getElementById("searchTerms").value
+
+  $.get("https://api.github.com/search/repositories?q=" + search, function(response){
+    var template = Handlebars.compile(document.getElementById("results-template").innerHTML)
+    var repoList= template(response)
+    document.getElementById("results").innerHTML = repoList
+  }).fail(function(error){
+    displayError();
+  })
+}
+
+function showCommits(el){
+  var repo = el.dataset.repo
+  var owner = el.dataset.owner
+
+  $.get("https://api.github.com/repos/" + owner + "/" + repo +"/commits", function(response){
+    var template = Handlebars.compile(document.getElementById("commits-template").innerHTML)
+    var detailList = template(response)
+    document.getElementById("details").innerHTML = detailList
+  }).fail(function(error){
+    displayError()
+  })
+}
+
+function displayError(){
+  document.getElementById("errors").innerHTML = "I'm sorry, there's been an error. Please try again."
+}
