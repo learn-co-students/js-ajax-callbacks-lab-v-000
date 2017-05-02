d23ec1ae4719d1df7b22dad2baee50abc75f041d
Bob Habig <rfhab2@gmail.com>
2017-05-01 23:56:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+
+
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function searchRepositories() {
+  var repoSearch = $('#searchTerms').val();
+  $.get(`https://api.github.com/search/repositories?q=${repoSearch}`, function(data) {
+    const template = Handlebars.compile($('#results-template').html());
+    $('#results').html(template(data));
+  }).fail(error => {
+    displayError();
+  })
+}
+
+function showCommits(el) {
+  var owner = el.dataset.owner;
+  var repo = el.dataset.repository;
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, function(data) {
+    const template = Handlebars.compile($('#commits-template').html());
+    $('#details').html(template(data));
+  });
+}
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
