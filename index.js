8171bf970a53c1619b8d9a22e2fd494d91b2b354
github username <github email address>
2017-05-01 22:58:05 -0400

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,29 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+function searchRepositories() {
+  var search = document.getElementById("searchTerms").value
+
+  $.get("https://api.github.com/search/repositories?q=" + search, function(response) {
+    var template = Handlebars.compile(document.getElementById("results-template").innerHTML)
+    var repoList = template(response)
+    document.getElementById("results").innerHTML = repoList
+  }).fail(function(error) {
+    displayError();
+  })
+}
+
+function displayError() {
+  document.getElementById("errors").innerHTML =  "I'm sorry, there's been an error. Please try again."
+}
+
+function showCommits(el) {
+  $.get("https://api.github.com/repos/" + el.dataset.owner + "/" + el.dataset.repository +"/commits", function(response) {
+    var template = Handlebars.compile(document.getElementById("commits-template").innerHTML)
+    var detailList = template(response)
+    document.getElementById("details").innerHTML = detailList
+  }).fail(function(error) {
+    displayError()
+  })
+}
