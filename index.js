c1c88b64f1df422b8372a59877738c52d8b2f809
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-05-04 12:48:59 -0400

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ $(document).ready(function (){
 });
 
 function searchRepositories(){
-    const search_string = document.getElementById("searchTerm").value;
+    const search_string = document.getElementById("searchTerms").value;
     $.get("https://api.github.com/search/repositories?q=" + search_string, function(data) {
         const source = $("#repository-template").html();
         const template = Handlebars.compile(source);
@@ -21,4 +21,18 @@ function searchRepositories(){
 }
 function displayError(e) {
     $("#errors").html("error")
+}
+
+function showCommits(el) {
+    const repo = el.dataset.repository;
+    const owner = el.dataset.owner;
+    $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, function(data) {
+        const source = $("#commits-template").html();
+        const template = Handlebars.compile(source);
+        
+        $("#details").html(template(data));
+        
+    }).fail(function(e){
+        displayError(e);
+    })
 }
