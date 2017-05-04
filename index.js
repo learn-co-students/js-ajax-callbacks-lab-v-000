e16f334befffe47bdfd9c85d8dfb8f98e2e26976
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-05-04 12:28:16 -0400

    Displaying search results

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+function searchRepositories(){
+    const search_string = document.getElementById("searchTerm").value;
+    $.get("https://api.github.com/search/repositories?q=" + search_string, function(data) {
+        const source = $("#repository-template").html();
+        const template = Handlebars.compile(source);
+        
+        $("#results").html(template(data.items));
+        
+    }).fail(function(e){
+        displayError(e);
+    })
+}
+function displayError(e) {
+    $("#errors").html("error")
+}
