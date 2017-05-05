c9671053000d8afbb08ce4683bf24c6066888453
Alex Glover <Alex@MacBook-Pro-3.local>
2017-05-05 17:21:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
+const rootURL = "https://api.github.com"
+const searchURL = "https://api.github.com/search/repositories?q="
+
 function handlebarsSetup() {
-  //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
 }
 
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+function searchRepositories() {
+  const searchTerms = document.getElementById("searchTerms").value
+  const uri = searchURL + searchTerms
+
+  $.get(uri, data => {
+    const template = Handlebars.compile($('#results-template').html())
+    $('#results').html(template(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+function showCommits(el) {
+  const owner = el.dataset.owner
+  const repo = el.dataset.repository
+  const uri = `${rootURL}/repos/${owner}/${repo}/commits`
+
+  $.get(uri, data => {
+    const template = Handlebars.compile($('#commits-template').html())
+    $('#details').html(template(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+function displayError() {
+  const error = "I'm sorry, there's been an error. Please try again."
+  document.getElementById('errors').innerHTML = error
+}
