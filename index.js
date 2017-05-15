8f97c30c9fd06e48c599d13ad9420f31c91cd6ed
github username <github email address>
2017-05-15 16:16:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+function displayError() {
+  $("#errors").html("I'm sorry, there's been an error. Please try again.")
+}
+
+function searchRepositories() {
+
+  var searchTerms = $("#searchTerms").val();
+  console.log(searchTerms)
+  $.get(`https://api.github.com//search/repositories?q=${searchTerms}`,  displayRepositories()).fail(displayError());
+}
+
+function displayRepositories(data) {
+
+}
+
+function showCommits() {
+
+}
+
+
 function handlebarsSetup() {
   //put any handlebars setup in here
-  Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
+  Handlebars.registerPartial("userDetails", $("#user-details-partial").html());
 }
 
 $(document).ready(function (){
