d7f69f6fb77b3a24c9842ad2337eb469377f08b4
bentonwong <bentonwong@gmail.com>
2017-05-23 13:56:24 -0700

    done

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+function searchRepositories() {
+  const query = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${query}`, data => {
+    const template = Handlebars.compile($('#results-template').html())
+    $('#results').html(template(data))
+  }).fail(function(error) {
+    displayError()
+  });
+}
+
+function showCommits(el) {
+  const name = el.dataset.repository
+  const owner = el.dataset.owner
+  $.get(`https://api.github.com/repos/${owner}/${name}/commits`, data => {
+      const template = Handlebars.compile($('#commits-template').html())
+      $('#details').html(template(data))
+    }).fail(function(error) {
+      displayError()
+  });
+}
+
+function displayError() {
+  $("#errors").html("I'm sorry, there's been an error. Please try again.");
+}
