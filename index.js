4aceffa4c468817edbf77ca027453b4f40eef389
WilliamMena <williammena1991@gmail.com>
2017-06-01 00:24:55 -0400

    Done.

--- index.js
+++ index.js
@@ -4,5 +4,38 @@ function handlebarsSetup() {
 }
 
 $(document).ready(function (){
-  handlebarsSetup()
+  handlebarsSetup(Handlebars.registerPartial('userDetails', document.getElementById("user-details-partial").innerHTML))
 });
+
+function searchRepositories() {
+  var searchTerms = document.getElementById("searchTerms").value
+  var url = `https://api.github.com/search/repositories?q=${searchTerms}`
+  $.get(url)
+    .done(function(data) {
+      const src = document.getElementById("repository-template").innerHTML
+      const template = Handlebars.compile(src)
+      const repoList = template(data.items)
+      document.getElementById("results").innerHTML = repoList
+    })
+}
+
+function showCommits(el) {
+  const owner = el.dataset.owner
+  const repo = el.dataset.repository
+
+  var url = `https://api.github.com/repos/${owner}/${repo}/commits`
+  $.get(url)
+    .done(function(data) {
+      const src = document.getElementById("commit-template").innerHTML
+      const template = Handlebars.compile(src)
+      const commitList = template(data)
+      document.getElementById("details").innerHTML = commitList
+    })
+    .fail(function(error) {
+      displayError();
+    })
+}
+
+function displayError() {
+  document.getElementById('errors').innerHTML += "I'm sorry, there's been an error. Please try again."
+}
