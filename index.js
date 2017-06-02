bbbbc1580206ab07e0e6fa63a447d9078852e843
jwolfe890 <jwolfe890@gmail.com>
2017-06-02 19:36:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+const rootURL = "https://api.github.com"
+
+function searchRepositories () { 
+  var query = document.getElementById("searchTerms").value
+$.get( `${rootURL}/search/repositories?q=${query}`, function( data ) { 
+  const template = Handlebars.compile($('#main-template').html())
+  $('#results').html(template(data))
+}).fail(function () {
+      displayError() 
+  })
+}
+
+function displayError () {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function showCommits (item) { 
+
+  debugger
+
+  const owner = item.dataset.owner
+  const repo = item.dataset.repository
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, function( data ) { 
+  const commits = Handlebars.compile($('#commits-partial').html())
+ $('#details').html(commits(data))
+}).fail(function () {
+      displayError() 
+  })
+}
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
@@ -5,4 +35,7 @@ function handlebarsSetup() {
 
 $(document).ready(function (){
   handlebarsSetup()
+
 });
+
+
