45a6964271f7326478fc853220f5cf63c72883ef
mxdavis <md01@me.com>
2017-06-18 10:03:02 +0300

    saved old, now will reset

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 $(document).ready(function (){
 });
+
+function searchRepositories(){
+  var searchTerms = document.getElementById("searchTerms").value
+  var url = `https://api.github.com/search/repositories?q=${searchTerms}`
+  var req = new XMLHttpRequest()
+  req.addEventListener("load", displayRepositories)
+  req.open("GET", url)
+  req.send()
+  return false;
+}
+
+function displayRepositories(data, event){
+  debugger
+}
+
+var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
