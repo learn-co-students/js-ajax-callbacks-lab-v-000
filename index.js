7316f8ee227054186d71372af140d005fe963ce1
Menachem Hornbacher <me@mendel.tech>
2017-07-03 16:22:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 $(document).ready(function (){
+
 });
+
+function displayError(){
+  $("#errors").html("error")
+}
+
+function searchRepositories(){
+  // https://api.github.com/search/repositories?q=tetris
+  const search_term = $("#searchTerms").val
+  $.get("https://api.github.com/search/repositories?q=" + search_term, (response) => {
+    console.log(response);
+    var results = response.items;
+    console.log(results);
+    results.forEach((result) => {
+      console.log(result.name, result.html_url);
+    });
+  }).fail((error) => {
+    // This is called when an error occurs
+    console.log('Something went wrong: ' + error);
+    displayError();
+  });
+}
