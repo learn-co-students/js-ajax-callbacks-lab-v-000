b9ca465417c147e09dd4d0be56b4c1f29e7b8c0b
Menachem Hornbacher <me@mendel.tech>
2017-07-03 16:56:56 +0000

    Done.

--- index.js
+++ index.js
@@ -8,17 +8,27 @@ function displayError(){
 
 function searchRepositories(){
   // https://api.github.com/search/repositories?q=tetris
-  const search_term = $("#searchTerms").val
+  const search_term = $("#searchTerms").val()
   $.get("https://api.github.com/search/repositories?q=" + search_term, (response) => {
-    console.log(response);
-    var results = response.items;
-    console.log(results);
-    results.forEach((result) => {
-      console.log(result.name, result.html_url);
+    var output = []
+    response.items.forEach((result) => {
+      output.push(`<li><a href=${result.html_url}>${result.name} - ${result.description}</a> - <a href="#" data-owner="${result.owner.login}" data-repository="${result.name}" onclick="showCommits(this)">see commits</a></li>`)
     });
+    $('#results').html("<ul>" + output.join("") + "</ul>")
   }).fail((error) => {
     // This is called when an error occurs
     console.log('Something went wrong: ' + error);
     displayError();
+  });
+}
+
+function showCommits(element){
+  $.get(`https://api.github.com/repos/${element.dataset.owner}/${element.dataset.repository}/commits`, (response) => {
+    var output = []
+    response.forEach((commit) => {
+      commit.author ? author = commit.author : author = {avatar_url: "", login: "N/A"}
+      output.push(`<li><p>${commit.sha}</p><p><img src="${author.avatar_url}" height="25"/> ${author.login}</p></li>`)
+    });
+    $('#details').html("<ul>" + output.join("") + "</ul>")
   });
 }
