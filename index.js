280ab14c4e7b6f184f21247644ffee36fe358ec7
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-07-04 12:52:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 $(document).ready(function (){
+
 });
+
+function searchRepositories(){
+  let query = $('#searchTerms').val().replace(' ', '+');
+  $.get("https://api.github.com/search/repositories?q="+query, (data)=>{
+    $('#results').html(Handlebars.compile($('#repositories-template').html())(data));
+  }).fail(displayError);
+}
+
+function showCommits(target){
+  const repo = target.dataset.repository, owner = target.dataset.owner;
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, (data)=>{
+    $('#details').html(Handlebars.compile($('#commits-template').html())(data));
+  }).fail(displayError);
+}
+
+function displayError(error){
+  $('#errors').append('I\'m sorry, there\'s been an error. Please try again.')
+}
