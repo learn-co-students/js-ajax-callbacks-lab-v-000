4f1d258a5fb6b5a367a79b1ed6b4b198cfd29795
nadinesk <nadine.khattak@gmail.com>
2017-07-17 11:48:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
+function displayError() {
+  $("#errors").html("I'm sorry, there's been an error. Please try again.")
+}
+
+
+function showCommits(el) {
+  
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, function(data) {
+    $("#details").html(
+      data.map(function(commit) {
+        return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+      }).join(''))
+  }).fail(function error() {
+      displayError()
+  })
+}
+function SearchResults(data) { 
+	return data.items.map(function(result) { 
+		return `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      <hr>
+    `
+	}); 
+}
+
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val()
+  const url = `https://api.github.com/search/repositories?q=${searchTerms}`
+  $.get(url, function(data) {
+      $('#results').html(SearchResults(data))
+    }).fail(error => {
+      displayError()
+    })
+}
+
+
 $(document).ready(function (){
+	
 });
