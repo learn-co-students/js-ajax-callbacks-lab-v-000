35c9db8f71ce8372e3ad843ce454926bb3213441
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-08-12 16:57:01 -0400

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -13,7 +13,10 @@ describe('index.js', () => {
   ]
 
   let $
-  jsdom({ html, scripts })
+  jsdom({
+    html,
+    scripts
+  })
 
   before(() => {
     window.$ = require('jquery')
@@ -41,7 +44,9 @@ describe('index.js', () => {
     before(() => {
       xhr = sinon.useFakeXMLHttpRequest()
       window.XMLHttpRequest = xhr
-      xhr.onCreate = function (req) { requests.push(req); }
+      xhr.onCreate = function(req) {
+        requests.push(req);
+      }
     })
     beforeEach(() => {
       requests = []
@@ -62,7 +67,12 @@ describe('index.js', () => {
 
     describe('showCommits', () => {
       it('calls the github commits api and displays results', () => {
-        const el = { dataset: { repository: "repo", owner: "owner" } }
+        const el = {
+          dataset: {
+            repository: "repo",
+            owner: "owner"
+          }
+        }
         showCommits(el)
         requests[0].respond(200, contentType, commitsData())
         expect(requests[0].url).toMatch(/https:\/\/api.github.com\/repos\/owner\/repo\/commits/)
@@ -72,11 +82,12 @@ describe('index.js', () => {
   })
 })
 
-const contentType = { "Content-Type": "application/json" }
+const contentType = {
+  "Content-Type": "application/json"
+}
 
 function commitsData() {
-  const commits = [
-  {
+  const commits = [{
     "url": "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
     "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e",
     "html_url": "https://github.com/octocat/Hello-World/commit/6dcb09b5b57875f334f61aebed695e2e4193db5e",
@@ -144,54 +155,50 @@ function commitsData() {
       "type": "User",
       "site_admin": false
     },
-    "parents": [
-      {
-        "url": "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-        "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e"
-        }
-      ]
-    }
-  ]
-  return(JSON.stringify(commits))
+    "parents": [{
+      "url": "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+      "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e"
+    }]
+  }]
+  return (JSON.stringify(commits))
 }
+
 function resultsData() {
   const results = {
     "total_count": 40,
     "incomplete_results": false,
-    "items": [
-      {
-        "id": 3081286,
-        "name": "Tetris",
-        "full_name": "dtrupenn/Tetris",
-        "owner": {
-          "login": "dtrupenn",
-          "id": 872147,
-          "avatar_url": "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
-          "gravatar_id": "",
-          "url": "https://api.github.com/users/dtrupenn",
-          "received_events_url": "https://api.github.com/users/dtrupenn/received_events",
-          "type": "User"
-        },
-        "private": false,
-        "html_url": "https://github.com/dtrupenn/Tetris",
-        "description": "A C implementation of Tetris using Pennsim through LC4",
-        "fork": false,
-        "url": "https://api.github.com/repos/dtrupenn/Tetris",
-        "created_at": "2012-01-01T00:31:50Z",
-        "updated_at": "2013-01-05T17:58:47Z",
-        "pushed_at": "2012-01-01T00:37:02Z",
-        "homepage": "",
-        "size": 524,
-        "stargazers_count": 1,
-        "watchers_count": 1,
-        "language": "Assembly",
-        "forks_count": 0,
-        "open_issues_count": 0,
-        "master_branch": "master",
-        "default_branch": "master",
-        "score": 10.309712
-      }
-    ]
+    "items": [{
+      "id": 3081286,
+      "name": "Tetris",
+      "full_name": "dtrupenn/Tetris",
+      "owner": {
+        "login": "dtrupenn",
+        "id": 872147,
+        "avatar_url": "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/dtrupenn",
+        "received_events_url": "https://api.github.com/users/dtrupenn/received_events",
+        "type": "User"
+      },
+      "private": false,
+      "html_url": "https://github.com/dtrupenn/Tetris",
+      "description": "A C implementation of Tetris using Pennsim through LC4",
+      "fork": false,
+      "url": "https://api.github.com/repos/dtrupenn/Tetris",
+      "created_at": "2012-01-01T00:31:50Z",
+      "updated_at": "2013-01-05T17:58:47Z",
+      "pushed_at": "2012-01-01T00:37:02Z",
+      "homepage": "",
+      "size": 524,
+      "stargazers_count": 1,
+      "watchers_count": 1,
+      "language": "Assembly",
+      "forks_count": 0,
+      "open_issues_count": 0,
+      "master_branch": "master",
+      "default_branch": "master",
+      "score": 10.309712
+    }]
   }
   return JSON.stringify(results)
 }
