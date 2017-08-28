71c406aae9a02f162c36588f5563b4687539f288
Luisa Scavo <luisascavo@gmail.com>
2017-08-28 16:48:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 $(document).ready(function (){
 });
+
+function searchRepositories() {
+  const searchTerms = $("#searchTerms").val().replace(" ", "+");
+  const baseURI = "https://api.github.com/search/repositories?q="
+  const searchURI = baseURI + searchTerms
+
+  const src = $("#search-results-template").html();
+  const template = Handlebars.compile(src);
+
+  // const src = document.getElementById("search-results-template").innerHTML;
+  // const template = Handlebars.compile(src);
+
+  $.get(searchURI, function(response){
+    console.log(response);
+    $("#results").html(template(response.items));
+  }).fail(function(error) {
+    displayError();
+  });
+}
+
+function displayError() {
+  $("#errors").html("I'm sorry, there's been an error. Please try again.")
+}
