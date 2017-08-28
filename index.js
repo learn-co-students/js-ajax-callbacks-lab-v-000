30f4992e4515129a8ad0f6164309fa5cfd5d3256
Luisa Scavo <luisascavo@gmail.com>
2017-08-28 17:34:01 +0000

    Done.

--- index.js
+++ index.js
@@ -23,3 +23,15 @@ function searchRepositories() {
 function displayError() {
   $("#errors").html("I'm sorry, there's been an error. Please try again.")
 }
+
+function showCommits(el) {
+  const src = $("#show-commits-template").html();
+  const template = Handlebars.compile(src);
+
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, function(response) {
+    console.log(response);
+    $("#details").html(template(response))
+  }).fail(function(error) {
+    displayError();
+  });
+}
