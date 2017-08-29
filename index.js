727007ec3198b75323d84aa2dd8d865fe0665fb2
denisedearman <dearmandenise@gmail.com>
2017-08-29 11:17:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-$(document).ready(function (){
-});
+function searchRepositories(){
+  var searchTerms = document.getElementById("searchTerms").value;
+
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data) {
+    const results = data.items;
+    const src = document.getElementById("repository-template").innerHTML;
+    const template = Handlebars.compile(src);
+    const repoList = template(results);
+    $("#results").html(repoList);
+  }).fail(function(error) {
+    displayError();
+  });
+
+}
+
+function displayError(){
+  $("#errors").html("I'm sorry, there's been an error. Please try again.")
+}
+
+function showCommits(el){
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, function(data) {
+    const commits = data;
+    const src = document.getElementById("commits-template").innerHTML;
+    const template = Handlebars.compile(src);
+    const commitList = template(commits);
+    $("#details").html(commitList);
+  }).fail(function(error) {
+    displayError();
+  });
+}
