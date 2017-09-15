5682a08d36cef500241007eccf9af5ff528954c3
github username <github email address>
2017-09-15 13:44:53 -0400

    Done.

--- index.js
+++ index.js
@@ -14,24 +14,24 @@ function searchRepositories(){
   console.log(searchTerms)
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
       displayRepository(data.items)
+    }).fail(error => {
+      displayError()
     })
 }
 
 
 function displayRepository(results){
   results.forEach(function(result){
-    $('#results').append(`
-                          <h1><a href='${result.html_url}'>${result.name}</a></h1>
-
-                          <p>${result.description}</p>
-                        `)
+    $('#results').append(
+      `<h1><a href='${result.html_url}'>${result.name}</a></h1>
+      <p>${result.description}</p>`)
   })
 }
 
-// function displayError(){
-//   $("errors").html("I'm sorry, there's been an error. Please try again.")
-// }
+function displayError(){
+  $("#errors").append("I'm sorry, there's been an error. Please try again.")
+}
 
-// function showCommits(){
+// function showCommits(el){
 //   $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`=> )
 // }
