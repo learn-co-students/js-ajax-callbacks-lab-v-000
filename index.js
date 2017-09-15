0b1eb8c714d39164ecc6113c5d1844f6c1c372dc
github username <github email address>
2017-09-15 10:24:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 $(document).ready(function (){
 });
+
+
+var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
+
+var searchRepositories = () => {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+      $('#results').html(renderSearchResults(data))
+    })
+}
+
+function displayRepository(result){
+
+}
+
+// function displayError(){
+//   $("errors").html("I'm sorry, there's been an error. Please try again.")
+// }
+
+// function showCommits(){
+//   $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`=> )
+// }
