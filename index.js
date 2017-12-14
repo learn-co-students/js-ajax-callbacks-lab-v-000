98a2f0a30d7c1bb7b1ceeae8c3cfbd1a03d2e14e
John Tuttle <thejwt@gmail.com>
2017-12-14 14:09:40 +0000

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,17 @@ function displayError() {
 
 function searchRepositories() {
   var searchTerms = document.getElementById("searchTerms").value
-  $.get('https://api.github.com/search/repositories/?=' + searchTerms)
+  $.get('/https:\/\/api.github.com\/search\/repositories\?q=' + searchTerms, function(response){
+    document.getElementById("results").innerHTML += response.items[0].name
+  })
+}
+
+function showCommits(el) {
+  const name = el.dataset.repository
+  const owner = el.dataset.owner
+  //console.log(name)
+  $.get('/https:\/\/api.github.com\/repos\/' + owner + '\/' + name + '\/commits/', function(response){
+    console.log(response[0].sha)
+    document.getElementById("details").innerHTML += response[0].sha
+  })
 }
