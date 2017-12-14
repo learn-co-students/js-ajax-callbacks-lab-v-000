fb1704078e363220de833f4eb0b5a059d2591054
John Tuttle <thejwt@gmail.com>
2017-12-14 13:50:25 +0000

    finished first 2 tests

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 $(document).ready(function (){
 });
+
+function displayError() {
+  document.getElementById("errors").innerHTML += "<h3>error</h3>";
+}
+
+function searchRepositories() {
+  var searchTerms = document.getElementById("searchTerms").value
+  $.get('https://api.github.com/search/repositories/?=' + searchTerms)
+}
