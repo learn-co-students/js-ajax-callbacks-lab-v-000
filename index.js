bf2a2f067f4ca0a0b3bceb197fa7b4f1fef58296
github username <github email address>
2019-01-03 20:38:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,69 @@
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val();
+
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, response => {
+    $('#results').html(renderRepositories(response))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+const renderRepositories = (repos) => {
+  return repos.items.map(repo => {
+    return renderRepository(repo)
+  })
+}
+
+const renderRepository = (repo) => {
+  let description 
+  if (repo.description === null) {
+    description = "No description available" 
+  }
+  else {
+    description = repo.description 
+  }
+  return (`
+    <div id="${repo.name}">
+    <a href="${repo.html_url}"><h2>${repo.name}</h2></a>
+    <p>Description: ${description}</p>
+    <p>Username:<a href="${repo.owner.html_url}">${repo.owner.login}</a></p>
+    <a href="#" data-repository="${repo.name}" data-owner="${repo.owner.login}" onclick="showCommits(this)">Show commits</a><br>
+    <img src="${repo.owner.avatar_url}" height="32" width="32">
+  </div>
+  `)
+}
+
+function showCommits(element) {
+  const commitsURL = `https://api.github.com/repos/${element.dataset.owner}/${element.dataset.repository}/commits`
+  $.get(commitsURL, response => {
+    console.log(response);
+    $('#details').html(renderCommits(response))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+
+const renderCommits = (commits) => {
+  return commits.map(commit => {
+    return renderCommit(commit)
+  })
+}
+
+const renderCommit = (commit) => {
+  return `
+    <div id="${commit.sha}">
+      <img src="${commit.author.avatar_url}" height="32" width="32">
+      <p>Author: ${commit.commit.author.name}</p>
+      <p>Username: ${commit.author.login}</p>
+      <p>Sha: ${commit.sha}</p>
+    </div>
+    `
+}
+
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
 $(document).ready(function (){
 });
